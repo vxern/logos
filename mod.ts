bd8b28514c21623ca34ce0933a5e64d05eb60e27
vxern <56321034+vxern@users.noreply.github.com>
2021-10-28 10:44:51 +0100

    Created entry 'mod.ts' to the project

---
+++ mod.ts
@@ -0,0 +1,28 @@
+import "https://deno.land/x/dotenv@v3.1.0/load.ts";
+import { colors, Intents } from "./deps.ts";
+import { Client } from "./src/client.ts";
+
+const required = ["DISCORD_SECRET"];
+const supplied = Object.fromEntries(
+  required.map((env) => [env, Deno.env.get(env)]),
+);
+
+if (Object.values(supplied).includes(undefined)) {
+  const missing = Object.fromEntries(
+    Object.entries(supplied).filter(([_, value]) => value === undefined),
+  );
+  console.error(
+    colors.red(
+      `Missing one or more environment variables:\n    ${
+        Object.keys(missing).join(", ")
+      }`,
+    ),
+  );
+  Deno.exit(1);
+}
+
+const client = new Client();
+client.connect(Deno.env.get("DISCORD_SECRET")!, [
+  ...Intents.Presence,
+  ...Intents.GuildMembers,
+]);
