9e487f31d292e2e4bece9e4b89f455939d71366f
vxern <56321034+vxern@users.noreply.github.com>
2021-10-28 11:20:39 +0100

    Added 'display' subcommand to 'volume' command

--- src/modules/music/commands/volume.ts
+++ src/modules/music/commands/volume.ts
@@ -8,8 +8,11 @@ const config = {
 
 const command: Command = {
   name: "volume",
-  description: "Displays the volume of playback.",
   options: [{
+    type: "SUB_COMMAND",
+    name: "display",
+    description: "Displays the volume of playback.",
+  }, {
     type: "SUB_COMMAND",
     name: "set",
     description: "Sets the volume of playback.",
@@ -18,7 +21,7 @@ const command: Command = {
       name: "volume",
       description: `A value between 0 and ${config.volume.maximum}`,
       required: true,
-    }],
+    }]
   }],
   execute: unimplemented,
 };
