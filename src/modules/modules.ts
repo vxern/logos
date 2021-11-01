68eab5dc45ce141fbab9d7a1436abaa2c659f781
vxern <56321034+vxern@users.noreply.github.com>
2021-11-01 08:34:17 +0000

    Added documentation to 'modules.ts'

--- src/modules/modules.ts
+++ src/modules/modules.ts
@@ -1,4 +1,4 @@
-import { Command, mergeCommands } from "./command.ts";
+import { Command, mergeCommandOptions } from "../commands/command.ts";
 import information from "./information/information.ts";
 import moderation from "./moderation/moderation.ts";
 import music from "./music/music.ts";
@@ -17,15 +17,23 @@ const modules: Record<string, Command>[] = [
 
 const commands = mergeModules(modules);
 
+/**
+ * Combines modules into a single array of {@link Command}s, merging commands
+ * with the same names but different options.
+ *
+ * @param modules - The modules to merge.
+ * @returns The array of merged {@link Command}s.
+ */
 function mergeModules(modules: Record<string, Command>[]): Command[] {
-  return Array<Command>().concat(
+  // Obtain the array of separate commands.
+  const commands = Array<Command>().concat(
     ...modules.map((module) => Object.values(module)),
-  ).filter((command, index, array) => {
-    const indexOfFirst = array.findIndex((first) =>
-      first.name === command.name
-    );
-    if (indexOfFirst !== index) {
-      array[indexOfFirst] = mergeCommands(array[indexOfFirst], command);
+  );
+  // Merge commands with the same name.
+  return commands.filter((command, index, array) => {
+    const firstIndex = array.findIndex((first) => first.name === command.name);
+    if (firstIndex !== index) {
+      array[firstIndex] = mergeCommandOptions([array[firstIndex], command]);
       return false;
     }
     return true;
