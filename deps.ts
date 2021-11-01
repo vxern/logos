b05a1ef59a0bfcea0bab183830ccb2bd9de311eb
vxern <56321034+vxern@users.noreply.github.com>
2021-11-01 08:35:13 +0000

    Added lodash to dependencies for utility functions

--- deps.ts
+++ deps.ts
@@ -1,2 +1,3 @@
 export * from "https://deno.land/x/harmony@v2.2.0/mod.ts";
 export * as colors from "https://deno.land/x/nanocolors@0.1.12/mod.ts";
+export * as _ from "https://deno.land/x/lodash@4.17.15-es/lodash.js";
