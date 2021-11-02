2425f1dd3c2697050544f94bb32849c93070cf29
vxern <56321034+vxern@users.noreply.github.com>
2021-11-02 22:09:25 +0000

    Added `supplyMissingProperties()` function to 'modules.ts'

--- src/modules/modules.ts
+++ src/modules/modules.ts
@@ -1,4 +1,10 @@
-import { Command, mergeCommandOptions } from "../commands/command.ts";
+import {
+  Command,
+  mergeCommandOptions,
+  unimplemented,
+} from "../commands/command.ts";
+import { Option, OptionType } from "../commands/option.ts";
+import config from "../config.ts";
 import information from "./information/information.ts";
 import moderation from "./moderation/moderation.ts";
 import music from "./music/music.ts";
@@ -30,14 +36,45 @@ function mergeModules(modules: Record<string, Command>[]): Command[] {
     ...modules.map((module) => Object.values(module)),
   );
   // Merge commands with the same name.
-  return commands.filter((command, index, array) => {
+  return supplyMissingProperties(commands.filter((command, index, array) => {
     const firstIndex = array.findIndex((first) => first.name === command.name);
     if (firstIndex !== index) {
       array[firstIndex] = mergeCommandOptions([array[firstIndex], command]);
       return false;
     }
     return true;
-  });
+  }));
+}
+
+/**
+ * Supplies properties which may be undefined with default values as Discord
+ * requires them to be truthy.
+ *
+ * @param elements - The array of {@link Command}s or {@link Option}s to whom to
+ * supply missing properties.
+ * @returns The modified array of {@link Command}s or {@link Option}s.
+ */
+function supplyMissingProperties<T extends Command | Option>(
+  elements: T[],
+): T[] {
+  for (const element of elements) {
+    element.description ??= config.failsafes.interactions.description;
+    switch (element.type) {
+      // An object represented by the Command interface will __not__ have the
+      // 'type' property assigned, therefore an [element.type] of `undefined`
+      // implies [element] is a command.
+      case undefined:
+        if (element.options && element.options.length > 0) break;
+      // Only options of type [OptionType.SUB_COMMAND] may have handlers.
+      case OptionType.SUB_COMMAND:
+        if (!element.handle) {
+          element.handle = unimplemented;
+        }
+    }
+    if (!element.options) continue;
+    supplyMissingProperties((element as Option).options!);
+  }
+  return elements;
 }
 
 export default { modules: modules, commands: commands };
