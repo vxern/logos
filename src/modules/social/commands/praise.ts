7a912e5946b61d008998b452df30fa060d2fcdcb
vxern <56321034+vxern@users.noreply.github.com>
2021-11-02 22:11:19 +0000

    Updated command definition and imports

--- src/modules/social/commands/praise.ts
+++ src/modules/social/commands/praise.ts
@@ -1,11 +1,10 @@
-import { Command, unimplemented } from "../../command.ts";
+import { Command } from "../../../commands/command.ts";
 import { user } from "../../parameters.ts";
 
 const command: Command = {
   name: "praise",
   description: "Praises a user for their contribution.",
   options: [user],
-  handle: unimplemented,
 };
 
 export default command;
