5497b15d2df46e73ca68601a396887f1fbadac28
vxern <56321034+vxern@users.noreply.github.com>
2021-11-10 23:03:35 +0000

    Updated module.ts

---
+++ src/modules/secret/module.ts
@@ -0,0 +1,45 @@
+import { CategoryChannel, Guild, GuildChannels } from "../../../deps.ts";
+import { Command } from "../../commands/command.ts";
+import compare from "./commands/compare.ts";
+
+const commands: Record<string, Command> = {
+  compare,
+};
+
+interface Category {
+  category: CategoryChannel;
+  channels: GuildChannels[];
+}
+
+interface GuildStructure {
+  channels: GuildChannels[];
+  categories: Category[];
+}
+
+async function analyseStructure(
+  guild: Guild,
+): Promise<GuildStructure> {
+  const channels = await guild.channels.array();
+  const categoryChannels = channels.filter((channel) =>
+    channel.isCategory()
+  ) as CategoryChannel[];
+  return {
+    channels: channels.filter((channel) =>
+      !(channel.parentID || channel.isCategory())
+    ),
+    categories: categoryChannels.map((
+      parentChannel,
+    ) => {
+      return {
+        category: parentChannel,
+        channels: channels.filter((channel) =>
+          channel.parentID === parentChannel.id
+        ),
+      };
+    }).sort((a, b) => a.category.position - b.category.position),
+  };
+}
+
+export type { Category, GuildStructure };
+export { analyseStructure };
+export default commands;
