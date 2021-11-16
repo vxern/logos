40251c04d1aef78825e54f1d84909acaa86cf54e
vxern <56321034+vxern@users.noreply.github.com>
2021-11-16 14:36:53 +0000

    Refactored code and added client options

--- src/client.ts
+++ src/client.ts
@@ -1,28 +1,49 @@
 import {
-  ApplicationCommand,
   ApplicationCommandInteraction,
-  ApplicationCommandOption,
-  ApplicationCommandPartial,
-  ApplicationCommandPermission,
-  ApplicationCommandPermissionType,
   ApplicationCommandType,
   Client as DiscordClient,
-  colors,
   event,
   Guild,
-  GuildSlashCommmandPermissionsPartial,
+  Intents,
 } from "../deps.ts";
-import { Command, unifyHandlers } from "./commands/command.ts";
+import {
+  Command,
+  createApplicationCommand,
+  createPermissions,
+  unifyHandlers,
+} from "./commands/command.ts";
 import modules from "./modules/modules.ts";
 import config from "./config.ts";
-import { areEqual } from "./utils.ts";
-import { Availability } from "./commands/availability.ts";
-import { resolveGuildRole } from "./modules/roles/structures/role.ts";
-import { roles } from "./modules/roles/module.ts";
 
 /** The core of the application, used for interacting with the Discord API. */
 class Client extends DiscordClient {
-  public static readonly managed: Map<string, string> = new Map();
+  public static readonly languages: Map<string, string> = new Map();
+
+  constructor() {
+    super({
+      id: Deno.env.get("APPLICATION_ID")!,
+      token: Deno.env.get("DISCORD_SECRET")!,
+      intents: Intents.GuildMembers,
+      forceNewSession: false,
+      presence: {
+        activity: {
+          name: "Deno",
+          type: "PLAYING",
+        },
+      },
+      canary: true,
+      clientProperties: {
+        os: "linux",
+        browser: "cards",
+        device: "cards",
+      },
+      enableSlash: true,
+      disableEnvToken: true,
+      fetchGatewayInfo: true,
+      compress: true,
+      messageCacheMax: 500,
+    });
+  }
 
   /**
    * Called when {@link Client} is authenticated by Discord, and is ready to use
@@ -33,56 +54,66 @@ class Client extends DiscordClient {
    */
   @event()
   protected async ready() {
-    const guilds = await this.guilds.array();
-    for (const guild of guilds) {
-      if (Client.isManagedGuild(guild)) {
-        Client.managed.set(
-          guild.id,
-          config.guilds.name.exec(guild.name!)![1].toLowerCase(),
-        );
-      }
-      await guild.roles.fetchAll();
-    }
+    const then = Date.now();
+    const promises = [
+      this.setupGuilds(),
+      this.setupCommands(),
+    ];
 
-    const commands = modules.commands;
-
-    console.info(
-      colors.cyan("Constructing handlers for commands with subcommands..."),
-    );
-    for (const command of commands) {
-      command.handle = unifyHandlers(command);
-    }
-
-    console.info(
-      colors.cyan("Assigning handlers to commands..."),
-    );
-    for (const command of commands) {
-      this.interactions.handle(command.name, (interaction) => {
-        console.info(
-          colors.magenta(
-            `Handling interaction '${
-              Client.displayCommand(interaction)
-            }' from ${colors.bold(interaction.user.tag)} on ${
-              colors.bold(interaction.guild!.name!)
-            }...`,
-          ),
-        );
-        command.handle!(interaction);
-      }, ApplicationCommandType.CHAT_INPUT);
-    }
-
-    for (const guild of guilds) {
-      await this.synchroniseGuildCommands(guild, commands);
-    }
-
-    this.setPresence({
-      activity: {
-        name: "Deno",
-        type: "PLAYING",
-      },
-    });
+    await Promise.all(promises);
+    const now = Date.now();
+    console.log(`Setup took ${now - then}ms`);
 
     this.notifyReady();
+  }
+
+  async setupGuilds(): Promise<unknown> {
+    const promises = [];
+
+    const guilds = await this.guilds.array();
+    for (const guild of guilds) {
+      promises.push(...[
+        guild.roles.fetchAll,
+        guild.members.fetchList,
+      ]);
+
+      if (Client.isManagedGuild(guild)) {
+        this.manageGuild(guild);
+      }
+    }
+
+    return Promise.all(promises);
+  }
+
+  manageGuild(guild: Guild): void {
+    const language = config.guilds.name.exec(guild.name!)![1].toLowerCase();
+
+    Client.languages.set(guild.id, language);
+  }
+
+  async setupCommands(): Promise<unknown> {
+    const commands = modules.commands;
+    for (const command of commands) {
+      command.handle = unifyHandlers(command);
+      this.manageCommand(command);
+    }
+
+    const promises = [];
+
+    const guilds = await this.guilds.array();
+    for (const guild of guilds) {
+      promises.push(this.synchroniseGuildCommands(guild, commands));
+    }
+
+    return Promise.all(promises);
+  }
+
+  manageCommand(command: Command) {
+    this.interactions.handle(
+      command.name,
+      (interaction) => command.handle!(interaction),
+      ApplicationCommandType.CHAT_INPUT,
+    );
   }
 
   /**
@@ -96,131 +127,19 @@ class Client extends DiscordClient {
   async synchroniseGuildCommands(
     guild: Guild,
     commands: Command[],
-  ): Promise<void> {
-    console.info(
-      colors.cyan(`Synchronising commands of ${colors.bold(guild.name!)}...`),
-    );
-
-    const guildCommands = (await guild.commands.all()).array();
-    const altered: [ApplicationCommand, Availability][] = [];
+  ): Promise<unknown> {
+    const applicationCommands = [];
     for (const command of commands) {
-      const commandPartial: ApplicationCommandPartial = {
-        name: command.name,
-        description: command.description,
-        options: command.options as ApplicationCommandOption[],
-        type: ApplicationCommandType.CHAT_INPUT,
-        defaultPermission: false,
-      };
-
-      const guildCommand = guildCommands.splice(
-        guildCommands.findIndex((guildCommand) =>
-          guildCommand.name === command.name
-        ),
-        1,
-      )[0];
-
-      if (!guildCommand) {
-        altered.push(
-          [
-            await this.interactions.commands.create(commandPartial, guild.id),
-            command.availability,
-          ],
-        );
-        continue;
-      }
-
-      if (!areEqual(guildCommand, command)) {
-        altered.push(
-          [
-            await this.interactions.commands.edit(
-              guildCommand.id,
-              commandPartial,
-              guild.id,
-            ),
-            command.availability,
-          ],
-        );
-      }
+      applicationCommands.push(createApplicationCommand(command));
     }
+    const guildCommands = await guild.commands.bulkEdit(applicationCommands);
 
-    const commandPermissions: GuildSlashCommmandPermissionsPartial[] = [];
-    for (const [command, availability] of altered) {
-      const permissions: ApplicationCommandPermission[] = [];
-      switch (availability) {
-        case Availability.EVERYONE: {
-          permissions.push({
-            id:
-              (await resolveGuildRole({ guild: guild, name: "@everyone" }))!.id,
-            type: ApplicationCommandPermissionType.ROLE,
-            permission: true,
-          });
-          break;
-        }
-        case Availability.MEMBERS: {
-          const proficiencies = roles.scopes.global.find((category) =>
-            category.name === "Proficiency"
-          )!.collection!.list!.map((role) => role.name);
-          for (const proficiency of proficiencies) {
-            // TODO(vxern): This code will throw an error in a guild without
-            // proficiency roles.
-            permissions.push({
-              id: (await resolveGuildRole({ guild: guild, name: proficiency }))!
-                .id,
-              type: ApplicationCommandPermissionType.ROLE,
-              permission: true,
-            });
-          }
-          break;
-        }
-        case Availability.GUIDES: {
-          permissions.push({
-            id:
-              (await resolveGuildRole({ guild: guild, name: roles.moderator }))!
-                .id,
-            type: ApplicationCommandPermissionType.ROLE,
-            permission: true,
-          });
-          break;
-        }
-        case Availability.OWNER: {
-          permissions.push({
-            id: config.guilds.owner.id,
-            type: ApplicationCommandPermissionType.USER,
-            permission: true,
-          });
-          break;
-        }
-      }
-      commandPermissions.push({
-        id: command.id,
-        permissions: permissions,
-      });
-    }
-    this.interactions.commands.permissions.bulkEdit(
-      commandPermissions,
-      guild.id,
+    const commandPermissions = await createPermissions(
+      guild,
+      guildCommands,
+      commands,
     );
-
-    if (altered.length !== 0) {
-      console.info(
-        colors.yellow(
-          `~ ${altered.map(([command, _]) => command.name).join(", ")}`,
-        ),
-      );
-    }
-
-    for (const guildCommand of guildCommands) {
-      this.interactions.commands.delete(
-        guildCommand.id,
-        guild.id,
-      );
-    }
-    const removed = guildCommands
-      .map((guildCommand) => guildCommand.name)
-      .join(", ");
-    if (removed.length !== 0) {
-      console.info(colors.red(`- ${removed}`));
-    }
+    return guild.commands.permissions.bulkEdit(commandPermissions);
   }
 
   /**
@@ -265,6 +184,16 @@ class Client extends DiscordClient {
   static displayCommand(command: ApplicationCommandInteraction): string {
     const parts = [command.name, command.subCommandGroup, command.subCommand];
     return parts.filter((part) => part).join(" ");
+  }
+
+  /**
+   * Returns the language of the guild.
+   *
+   * @param guild - The guild whose language to return.
+   * @returns The guild's language.
+   */
+  static getLanguage(guild: Guild) {
+    return Client.languages.get(guild.id);
   }
 }
 
