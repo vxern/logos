f8fb209e6864bbea2adc170b20cac74d2cb48dba
vxern <56321034+vxern@users.noreply.github.com>
2021-11-16 21:16:55 +0000

    Added `availability` property to 'profile' command

--- src/modules/social/commands/profile.ts
+++ src/modules/social/commands/profile.ts
@@ -1,9 +1,11 @@
+import { Availability } from "../../../commands/availability.ts";
 import { Command } from "../../../commands/command.ts";
 import { OptionType } from "../../../commands/option.ts";
 import { user } from "../../parameters.ts";
 
 const command: Command = {
   name: "profile",
+  availability: Availability.MEMBERS,
   options: [{
     name: "view",
     type: OptionType.SUB_COMMAND,
