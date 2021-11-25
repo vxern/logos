cd6602f3d55c3f1391d52f68165b26c9dc891910
vxern <56321034+vxern@users.noreply.github.com>
2021-11-25 11:01:41 +0000

    Created `Controller` class to store information shared between commands

---
+++ src/modules/controller.ts
@@ -0,0 +1,11 @@
+import { Guild } from "../../deps.ts";
+
+abstract class Controller {
+  readonly guild: Guild;
+
+  constructor(guild: Guild) {
+    this.guild = guild;
+  }
+}
+
+export { Controller };
