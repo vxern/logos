91e19d17b32276e51ca1024686d711b3ba0d09bd
vxern <56321034+vxern@users.noreply.github.com>
2022-01-03 11:16:31 +0000

    Created 'resources' command for displaying resources for learning the language.

---
+++ src/modules/language/commands/resources.ts
@@ -0,0 +1,29 @@
+import { Interaction } from "../../../../deps.ts";
+import { Client } from "../../../client.ts";
+import { Availability } from "../../../commands/availability.ts";
+import { Command } from "../../../commands/command.ts";
+import { fromHex } from "../../../utils.ts";
+
+const command: Command = {
+  name: "resources",
+  availability: Availability.MEMBERS,
+  description: "Displays a list of resources to learn the language.",
+  handle: resources,
+};
+
+function resources(interaction: Interaction): void {
+  const language = Client.getLanguage(interaction.guild!);
+
+  interaction.respond({
+    embeds: [{
+      title: "Resources",
+      description: language
+        ? `Resources to learn the ${language} language are available [here](https://github.com/vxern/${language?.toLowerCase()}).`
+        : "This server does not have a designated language, and therefore no resources are available for it.",
+      color: fromHex("#d6e3f8"),
+    }],
+    ephemeral: true,
+  });
+}
+
+export default command;
