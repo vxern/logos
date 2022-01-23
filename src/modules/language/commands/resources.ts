a55ab5e614746186bac855570aacefe0398cbfe7
vxern <56321034+vxern@users.noreply.github.com>
2022-01-23 16:25:57 +0000

    Added information about contributing to the repository in 'resources'.

--- src/modules/language/commands/resources.ts
+++ src/modules/language/commands/resources.ts
@@ -2,7 +2,8 @@ import { Interaction } from "../../../../deps.ts";
 import { Client } from "../../../client.ts";
 import { Availability } from "../../../commands/availability.ts";
 import { Command } from "../../../commands/command.ts";
-import { capitalise } from "../../../formatting.ts";
+import configuration from "../../../configuration.ts";
+import { capitalise, mention, MentionType } from "../../../formatting.ts";
 import { fromHex } from "../../../utils.ts";
 
 const command: Command = {
@@ -19,7 +20,11 @@ function resources(interaction: Interaction): void {
     embeds: [{
       title: "Resources",
       description: language
-        ? `Resources to learn the ${capitalise(language)} language are available [here](https://github.com/vxern/${language?.toLowerCase()}).`
+        ? `Resources to learn the ${capitalise(language)} language are available [here](https://github.com/vxern/${language?.toLowerCase()}).
+
+Feel free to contribute to the project by forking the repository, adding your own resources, and creating a pull request.
+
+If you don't know how to use git, you can still contribute by listing the resources and tagging ${mention(configuration.guilds.owner.id, MentionType.USER)}.`
         : "This server does not have a designated language, and therefore no resources are available for it.",
       color: fromHex("#d6e3f8"),
     }],
