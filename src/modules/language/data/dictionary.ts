7ad94fd284cd4e00329418e599586958250eaa1b
vxern <56321034+vxern@users.noreply.github.com>
2022-01-26 00:27:24 +0000

    Created dictionary and appropriate related structures.

---
+++ src/modules/language/data/dictionary.ts
@@ -0,0 +1,77 @@
+/** The language scope / audience of the dictionary. */
+enum DictionaryScope {
+  /** 
+   * The dictionary provides definitions in the same language as the headword.
+   */
+  MONOLINGUAL,
+  /** 
+   * The dictionary provides definitions in a different language to the
+   * headword.
+   */
+  BILINGUAL,
+  /**
+   * The dictionary provides definitions in multiple different languages to
+   * the headword.
+   */
+  MULTILINGUAL,
+  /**
+   * The dictionary provides definitions in a very large number of different
+   * languages to the headword.
+   */
+  OMNILINGUAL,
+}
+
+/** The type of the dictionary. */
+enum DictionaryType {
+  /** The dictionary furnishes definitions to the headword. */
+  DEFINING,
+  /** The dictionary furnishes the headword's etymology. */
+  ETYMOLOGICAL,
+  /** The dictionary furnishes the pronunciation of the headword. */
+  PHONETIC,
+  /** The dictionary furnishes synonyms of the headword in the language. */
+  SYNONYM,
+  /** The dictionary furnishes antonyms of the headword in the language. */
+  ANTONYM,
+}
+
+/** Represents a headword entry in a dictionary. */
+interface DictionaryEntry {
+  /** The headword of the entry. */
+  headword: string;
+  /** Translations of the entry. */
+  translations?: string[];
+  /** The pronunciation of the headword. */
+  pronunciation?: string;
+  /** The definition of the entry. */
+  definition?: string;
+  /** The etymology of the headword. */
+  etymology?: string;
+  /** Synonyms of the headword. */
+  synonyms?: string[];
+  /** Antonyms of the headword. */
+  antonyms?: string[];
+}
+
+type PartialDictionaryEntry = Omit<DictionaryEntry, "headword">;
+
+interface SearchQuery {
+  /** The headword to search for. */
+  word: string;
+  /** The language of the headword. */
+  native?: string;
+  /** The language of the translation or definition. */
+  target?: string;
+}
+
+abstract class Dictionary {
+  abstract scopes: DictionaryScope[];
+  abstract types: DictionaryType[];
+
+  abstract query: (query: SearchQuery) => string;
+
+  abstract lookup(headword: string, native: string): Promise<PartialDictionaryEntry>;
+}
+
+export { Dictionary, DictionaryScope, DictionaryType };
+export type { DictionaryEntry, PartialDictionaryEntry, SearchQuery }
