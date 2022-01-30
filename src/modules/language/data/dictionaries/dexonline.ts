89ed5509bbf0e00295fa1d302c91b3f666f74f1e
vxern <56321034+vxern@users.noreply.github.com>
2022-01-30 00:00:10 +0000

    Added 'dexonline.ro' to dictionaries.

---
+++ src/modules/language/data/dictionaries/dexonline.ts
@@ -0,0 +1,24 @@
+import { Dictionary, PartialDictionaryEntry, DictionaryScope, DictionaryType, SearchQuery } from '../dictionary.ts';
+
+class Dexonline extends Dictionary {
+  scopes = [DictionaryScope.MONOLINGUAL];
+  types = [DictionaryType.DEFINING, DictionaryType.ETYMOLOGICAL];
+
+  query = (query: SearchQuery) => `https://dexonline.ro/definitie/${query.word}/json`;
+
+  async lookup(word: string): Promise<PartialDictionaryEntry> {
+    const response = await fetch(this.query({ word }));
+    const content = await response.text();
+    const data = JSON.parse(content);
+
+    const raw = data.definitions[0].internalRep;
+    const definition = raw.replaceAll('#', '__')
+      .replaceAll('@', '**')
+      .replaceAll('%', '**')
+      .replaceAll('$', '*');
+
+    return { definition: definition };
+  }
+}
+
+export { Dexonline };
