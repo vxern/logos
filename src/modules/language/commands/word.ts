a3bff907621d94adc2e8450e228af1b5c6bff467
vxern <56321034+vxern@users.noreply.github.com>
2022-02-13 14:27:13 +0000

    Changed default language from Romanian to English, added more log messages.

--- src/modules/language/commands/word.ts
+++ src/modules/language/commands/word.ts
@@ -3,6 +3,7 @@ import { Client } from "../../../client.ts";
 import { Availability } from "../../../commands/availability.ts";
 import { Command } from "../../../commands/command.ts";
 import { OptionType } from "../../../commands/option.ts";
+import configuration from "../../../configuration.ts";
 import { capitalise } from "../../../formatting.ts";
 import { fromHex } from "../../../utils.ts";
 import { DictionaryEntry, toFields } from "../data/dictionary.ts";
@@ -35,26 +36,42 @@ async function word(interaction: Interaction): Promise<void> {
   const verbose = data.options.find((option) => option.name === "verbose")?.value ?? true;
   const show = data.options.find((option) => option.name === "show")?.value ?? false;
 
+  const language = Client.getLanguage(interaction.guild!) || 'english';
+  const dictionaries = Object.values(languages[language] ?? {});
+  const hasDictionary = dictionaries.length > 0;
+  
   const response = await interaction.respond({
     type: InteractionResponseType.DEFERRED_CHANNEL_MESSAGE,
-    ephemeral: !show
+    ephemeral: !hasDictionary || !show,
   });
 
-  const language = Client.getLanguage(interaction.guild!) || 'romanian';
-
-  console.log(`${interaction.user.username} is looking up a word '${word}' in ${capitalise(language)}.`);
+  if (dictionaries.length === 0) {
+    console.log(`${interaction.user.username} attempted to look up '${word}' in ${capitalise(language)}, but there are no available dictionaries for the language.`);
+    response.editResponse({
+      embeds: [{
+        title: "No available dictionaries.",
+        description: `There are no dictionary adapters installed for the ${capitalise(language)} language.`,
+        color: configuration.responses.colors.red,
+      }]
+    });
+    return;
+  }
 
   let entry: DictionaryEntry = {headword: word}; 
 
-  Object.values(languages[language]).map((dictionary) => dictionary.lookup(word, language).then((result) => {
+  dictionaries.map((dictionary) => dictionary.lookup(word, language).then((result) => {
     entry = {...result, ...entry};
 
     const fields = toFields(entry, {verbose: verbose});
+    const hasEntry = fields.length > 0;
+    if (fields.length === 0) {
+      console.log(`${interaction.user.username} attempted to look up '${word}' in ${capitalise(language)}, but no results came up.`);
+    }
 
     response.editResponse({
       embeds: [{
         title: entry.headword,
-        description: fields.length === 0 ? `No results found for '${entry.headword}'.` : undefined,
+        description: !hasEntry ? `No results found for '${entry.headword}'.` : undefined,
         fields: fields,
         color: fromHex("#d6e3f8"),
       }],
