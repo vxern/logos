3998828e21e0eb707072ab47e1b02a9d28863eed
vxern <56321034+vxern@users.noreply.github.com>
2022-02-15 12:25:54 +0000

    Added more colours to the configuration.

--- src/configuration.ts
+++ src/configuration.ts
@@ -9,6 +9,9 @@ export default {
   responses: {
     colors: {
       red: fromHex("#b42f2f"),
+      green: fromHex('#89ef59'),
+      blue: fromHex('#6269ed'),
+      yellow: fromHex('#f2f277'),
       invisible: fromHex("#36393F"),
     },
   },
