c8bf16738cb88032515a5ec83d1ef91c3d66d9ee
vxern <56321034+vxern@users.noreply.github.com>
2022-03-03 11:48:34 +0000

    Added data structures for sentence pairs and sentence selections.

---
+++ src/modules/language/data/sentence.ts
@@ -0,0 +1,22 @@
+/** Represents a pair of a sentence and its translation. */
+interface SentencePair {
+  /** The source sentence. */
+  sentence: string;
+  /** The translation of the sentence. */
+  translation: string;
+}
+
+/**
+ * Represents a selection of a sentence to be used for the language game of
+ * picking the correct word to fit into the blank space.
+ */
+interface SentenceSelection {
+  /** The selected sentence pair. */
+  pair: SentencePair;
+  /** The word which fits into the blank in the word. */
+  word: string;
+  /** Words to choose from to fit into the blank. */
+  choices: string[];
+}
+
+export type { SentencePair, SentenceSelection };
