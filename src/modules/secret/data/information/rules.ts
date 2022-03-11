352c833db67a408af71a3bbbff9d265182e44d87
vxern <56321034+vxern@users.noreply.github.com>
2022-03-11 03:40:00 +0000

    Improved phrasing of rules.

--- src/modules/secret/data/information/rules.ts
+++ src/modules/secret/data/information/rules.ts
@@ -14,37 +14,32 @@ const rules: Rules = {
 	behavior: (_) => {
 		return {
 			summary: 'Try to be nice.',
-			paragraph:
-				'All members should be treated with respect, consideration and understanding. Abuse, discrimination, harassment and other forms of hurtful or toxic behavior will not be tolerated.',
+			paragraph: 'It is expected of members to treat each other with respect, consideration and understanding. Malicious behaviour in the form of verbal abuse, discrimination, harassment and other forms of hurtful or toxic behaviour will not be tolerated.',
 		};
 	},
 	quality: async (guild: Guild) => {
 		const memesChannel = await getChannelMention(guild, 'memes');
 		return {
-			summary: 'Do not troll.',
-			paragraph:
-				`Contributions made to the server should be of decent quality. Trolling, spamming, flooding, shitposting (outside of the ${memesChannel} channel) and other forms of low-quality and annoying behavior will not be tolerated.`,
+			summary: 'Do not be obnoxious.',
+			paragraph: `It is expected of contributions made to the server to be of decent quality. Trolling, spamming, flooding, shitposting (outside of the ${memesChannel} channel) and other forms of annoying behaviour are sorely discouraged.`,
 		};
 	},
 	relevance: (_) => {
 		return {
 			summary: 'Post relevant content.',
-			paragraph:
-				'Contributions made to the server should be placed in their relevant channel and category. Contributions which are made in inappropriate channels will be asked to be moved to their relevant channel.',
+			paragraph: 'It is expected of contributions made to the server to be placed in their relevant channel and category. Contributions made in inappropriate channels for their subject will be asked to be moved to their relevant channel.',
 		};
 	},
 	suitability: (_) => {
 		return {
 			summary: 'Post content that is appropriate.',
-			paragraph:
-				'Contributions made to the server must be suitable for minors. NSFW and NSFL content will not be tolerated.',
+			paragraph: `It is expected of contributions made to the server to be appropriate for viewing by minors. Age-restricted (NSFW) and NSFL content is strictly prohibited. If you wouldn't show it to a minor, you shouldn't post it.`
 		};
 	},
 	exclusivity: (_) => {
 		return {
 			summary: 'Do not advertise.',
-			paragraph:
-				'Advertising of other Discord servers is not allowed, and any attempts at advertising (including profile descriptions) will not be tolerated.',
+			paragraph: 'It is expected of members to not advertise other Discord servers, and active attempts at advertising (including unsolicited DMs) are prohibited.',
 		};
 	},
 };
