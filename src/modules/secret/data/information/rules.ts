3b724dfcea811adf364e15935912a1004faf84c1
vxern <56321034+vxern@users.noreply.github.com>
2022-03-13 13:49:59 +0000

    Fixed formatting.

--- src/modules/secret/data/information/rules.ts
+++ src/modules/secret/data/information/rules.ts
@@ -14,32 +14,37 @@ const rules: Rules = {
 	behavior: (_) => {
 		return {
 			summary: 'Try to be nice.',
-			paragraph: 'It is expected of members to treat each other with respect, consideration and understanding. Malicious behaviour in the form of verbal abuse, discrimination, harassment and other forms of hurtful or toxic behaviour will not be tolerated.',
+			paragraph:
+				'It is expected of members to treat each other with respect, consideration and understanding. Malicious behaviour in the form of verbal abuse, discrimination, harassment and other forms of hurtful or toxic behaviour will not be tolerated.',
 		};
 	},
 	quality: async (guild: Guild) => {
 		const memesChannel = await getChannelMention(guild, 'memes');
 		return {
 			summary: 'Do not be obnoxious.',
-			paragraph: `It is expected of contributions made to the server to be of decent quality. Trolling, spamming, flooding, shitposting (outside of the ${memesChannel} channel) and other forms of annoying behaviour are sorely discouraged.`,
+			paragraph:
+				`It is expected of contributions made to the server to be of decent quality. Trolling, spamming, flooding, shitposting (outside of the ${memesChannel} channel) and other forms of annoying behaviour are sorely discouraged.`,
 		};
 	},
 	relevance: (_) => {
 		return {
 			summary: 'Post relevant content.',
-			paragraph: 'It is expected of contributions made to the server to be placed in their relevant channel and category. Contributions made in inappropriate channels for their subject will be asked to be moved to their relevant channel.',
+			paragraph:
+				'It is expected of contributions made to the server to be placed in their relevant channel and category. Contributions made in inappropriate channels for their subject will be asked to be moved to their relevant channel.',
 		};
 	},
 	suitability: (_) => {
 		return {
 			summary: 'Post content that is appropriate.',
-			paragraph: `It is expected of contributions made to the server to be appropriate for viewing by minors. Age-restricted (NSFW) and NSFL content is strictly prohibited. If you wouldn't show it to a minor, you shouldn't post it.`
+			paragraph:
+				`It is expected of contributions made to the server to be appropriate for viewing by minors. Age-restricted (NSFW) and NSFL content is strictly prohibited. If you wouldn't show it to a minor, you shouldn't post it.`,
 		};
 	},
 	exclusivity: (_) => {
 		return {
 			summary: 'Do not advertise.',
-			paragraph: 'It is expected of members to not advertise other Discord servers, and active attempts at advertising (including unsolicited DMs) are prohibited.',
+			paragraph:
+				'It is expected of members to not advertise other Discord servers, and active attempts at advertising (including unsolicited DMs) are prohibited.',
 		};
 	},
 };
