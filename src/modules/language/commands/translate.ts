880d5f8074de7c844455304c6e5b3eade8362863
vxern <vxern@wordcollector.co.uk>
2022-04-05 14:59:28 +0100

    Fixed translation service and added options for both the source and target languages, based on the list of supported languages dynamically fetched from DeepL.

--- src/modules/language/commands/translate.ts
+++ src/modules/language/commands/translate.ts
@@ -3,17 +3,35 @@ import {
 	InteractionApplicationCommandData,
 	InteractionResponseType,
 } from '../../../../deps.ts';
-import { Client } from '../../../client.ts';
+import secrets from '../../../../secrets.ts';
 import { Availability } from '../../../commands/availability.ts';
 import { Command } from '../../../commands/command.ts';
 import { OptionType } from '../../../commands/option.ts';
 import configuration from '../../../configuration.ts';
-import { capitalise } from '../../../formatting.ts';
-import {
-	addParametersToURL,
-	getLanguage,
-	getLanguageCode,
-} from '../../../utils.ts';
+import { addParametersToURL, getLanguage } from '../../../utils.ts';
+
+interface SupportedLanguage {
+	language: string;
+	name: string;
+	supports_formality: boolean;
+}
+
+const supportedLanguagesRequest = await fetch(
+	addParametersToURL('https://api-free.deepl.com/v2/languages', {
+		'auth_key': secrets.modules.language.deepL.secret,
+		'type': 'target',
+	}),
+);
+const supportedLanguages =
+	(await supportedLanguagesRequest.json()) as SupportedLanguage[];
+const supportedLanguagesChoices = supportedLanguages.map(
+	(supportedLanguage) => {
+		return {
+			name: supportedLanguage.name,
+			value: supportedLanguage.language,
+		};
+	},
+);
 
 const command: Command = {
 	name: 'translate',
@@ -21,6 +39,18 @@ const command: Command = {
 	description:
 		'Translates a given text from the server language to English or vice-versa.',
 	options: [{
+		name: 'from',
+		description: 'The source language.',
+		required: true,
+		choices: supportedLanguagesChoices,
+		type: OptionType.STRING,
+	}, {
+		name: 'to',
+		description: 'The target language.',
+		required: true,
+		choices: supportedLanguagesChoices,
+		type: OptionType.STRING,
+	}, {
 		name: 'text',
 		description: 'The text to translate.',
 		required: true,
@@ -34,12 +64,6 @@ const command: Command = {
 	handle: translate,
 };
 
-interface LanguageResponse {
-	language: string;
-	name: string;
-	supports_formality: boolean;
-}
-
 interface TranslationResponse {
 	detected_source_language: string;
 	text: string;
@@ -47,7 +71,9 @@ interface TranslationResponse {
 
 async function translate(interaction: Interaction): Promise<void> {
 	const data = interaction.data! as InteractionApplicationCommandData;
-	const text = data.options[0].value! as string;
+	const sourceCode = data.options[0].value! as string;
+	const targetCode = data.options[1].value! as string;
+	const text = data.options[2].value! as string;
 	const show = data.options.find((option) => option.name === 'show')?.value ??
 		false;
 
@@ -56,73 +82,37 @@ async function translate(interaction: Interaction): Promise<void> {
 		ephemeral: !show,
 	});
 
-	const language = Client.getLanguage(interaction.guild!);
-
-	const supportedLanguagesRequest = await fetch(
-		addParametersToURL('https://api-free.deepl.com/v2/languages', {
-			'auth_key': Deno.env.get(
-				'DEEPL_SECRET',
-			)!,
-			'type': 'target',
-		}),
-	);
-	const supportedLanguagesJson = await supportedLanguagesRequest.json();
-	const supportedLanguages: string[] = supportedLanguagesJson.map((
-		language: LanguageResponse,
-	) => language.name.split(' ')[0]);
-
-	if (!supportedLanguages.includes(capitalise(language))) {
-		response.editResponse({
-			embeds: [{
-				title: 'Unsupported language.',
-				description:
-					`The ${capitalise(language)} language does not have support for translation yet.`,
-				color: configuration.responses.colors.red,
-			}],
-		});
-		return;
-	}
-
-  const detectionRequest = await fetch(
-    addParametersToURL('https://language-detection10.p.rapidapi.com/detect_language', {'text': text}),
-    {
-      method: 'GET',
-      headers: {
-        'x-rapidapi-host': 'language-detection10.p.rapidapi.com',
-        'x-rapidapi-key': Deno.env.get('LANGUAGE_DETECTION_SECRET')!,
-      },
-    }
-  );
-  const detectionJson = await detectionRequest.json();
-  const sourceCode = detectionJson.language!;
-  const targetCode = sourceCode === 'en' ? getLanguageCode(language) : 'en';
-
 	const translationRequest = await fetch(
 		addParametersToURL('https://api-free.deepl.com/v2/translate', {
-			'auth_key': Deno.env.get('DEEPL_SECRET')!,
+			'auth_key': secrets.modules.language.deepL.secret,
 			'text': text,
-			'target_lang': targetCode.toUpperCase(),
+			'source_lang': sourceCode,
+			'target_lang': targetCode,
 		}),
 	);
 	const translationJson = await translationRequest.json();
 	const translation = translationJson.translations[0] as TranslationResponse;
 
-  const source = getLanguage(sourceCode);
-  const target = getLanguage(targetCode);
+	const source = supportedLanguages.find((supportedLanguage) =>
+		supportedLanguage.language === sourceCode
+	)!;
+	const target = supportedLanguages.find((supportedLanguage) =>
+		supportedLanguage.language === targetCode
+	)!;
 
 	response.editResponse({
 		embeds: [{
 			title: `${source} → ${target}`,
 			color: configuration.responses.colors.blue,
-      fields: [{
-			  name: source,
-			  value: text,
-        inline: false,
-      }, {
-			  name: target,
-			  value: translation.text,
-        inline: false,
-      }]
+			fields: [{
+				name: source.language,
+				value: text,
+				inline: false,
+			}, {
+				name: target.language,
+				value: translation.text,
+				inline: false,
+			}],
 		}],
 	});
 }
