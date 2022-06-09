3eab81266eb230d170621e23db0eb19dd752ce64
vxern <vxern@wordcollector.co.uk>
2022-06-09 20:25:10 +0100

    chore: Add `dayjs` to exports.

--- deps.ts
+++ deps.ts
@@ -15,3 +15,10 @@ export * as colors from 'https://deno.land/x/nanocolors@0.1.12/mod.ts';
 
 /** Utility features. */
 export * as _ from 'https://deno.land/x/lodash@4.17.15-es/lodash.js';
+
+/** Date/time library for dealing with time. */
+import dayjs from 'https://cdn.skypack.dev/dayjs';
+import relativeTime from 'https://cdn.skypack.dev/dayjs/plugin/relativeTime';
+dayjs.extend(relativeTime);
+
+export { dayjs };
