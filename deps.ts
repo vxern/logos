ac14fc1e8f1a5ea5601547f2bba9cc75a0399116
vxern <vxern@wordcollector.co.uk>
2022-06-09 20:22:54 +0100

    docs: Add documentation to package exports.

--- deps.ts
+++ deps.ts
@@ -1,6 +1,17 @@
+/** Discord API library. */
 export * from 'https://raw.githubusercontent.com/harmonyland/harmony/main/mod.ts';
-export * as colors from 'https://deno.land/x/nanocolors@0.1.12/mod.ts';
-export * as _ from 'https://deno.land/x/lodash@4.17.15-es/lodash.js';
-export * from 'https://deno.land/x/cheerio@1.0.4/mod.ts';
-export * as lavadeno from 'https://deno.land/x/lavadeno@3.2.2/mod.ts';
+
+/** Database connection. */
 export * as faunadb from 'https://deno.land/x/fauna@5.0.0-deno-alpha9/mod.ts';
+
+/** Music playback. */
+export * as lavadeno from 'https://deno.land/x/lavadeno@3.2.2/mod.ts';
+
+/** Extracting data from webpages. */
+export * from 'https://deno.land/x/cheerio@1.0.4/mod.ts';
+
+/** Colouring console output. */
+export * as colors from 'https://deno.land/x/nanocolors@0.1.12/mod.ts';
+
+/** Utility features. */
+export * as _ from 'https://deno.land/x/lodash@4.17.15-es/lodash.js';
