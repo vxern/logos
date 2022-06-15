df92a7b9ae22bc657e950330c9fd70d86e5acc58
vxern <vxern@wordcollector.co.uk>
2022-06-15 14:05:44 +0100

    feat: Add configuration settings pertaining to article creation and editing.

--- src/configuration.ts
+++ src/configuration.ts
@@ -47,7 +47,7 @@ export default {
 				footer: {
 					label: 'Additional information / notes',
 					style: TextInputStyle.PARAGRAPH,
-          required: false,
+					required: false,
 					minimum: 10,
 					maximum: 500,
 				},
@@ -102,6 +102,20 @@ export default {
 				requiresVerification: false,
 			},
 		},
+		articles: {
+			create: {
+				// The maximum number of articles that can be created...
+				maximum: 3,
+				// ... in the interval:
+				interval: 60 * 60 * 24 * 1000, // One day.
+			},
+			edit: {
+				// The maximum number of edits that can be made...
+				maximum: 5,
+				// ... in the interval:
+				interval: 60 * 60 * 12 * 1000, // 12 hours.
+			},
+		},
 		// Configuration settings pertaining to the de facto owner of the managed guilds.
 		owner: {
 			id: '217319536485990400',
@@ -112,7 +126,7 @@ export default {
 		},
 		// Configuration settings pertaining to entry to the guild.
 		entry: {
-			minimumRequiredAge: 60 * 60 * 24 * 1000, // Two days.
+			minimumRequiredAge: 2 * 60 * 60 * 24 * 1000, // Two days.
 		},
 	},
 };
