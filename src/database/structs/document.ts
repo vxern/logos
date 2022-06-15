69a86682ffb48e84f1c1ba2b21332827cfbfa824
vxern <vxern@wordcollector.co.uk>
2022-06-15 14:01:19 +0100

    refactor: Introduce `Reference` interface for document references.

--- src/database/structs/document.ts
+++ src/database/structs/document.ts
@@ -1,13 +1,24 @@
 /** Contains the base properties of data structures stored in the database. */
 interface Document<T> {
-	/** Reference to this resource in the database. */
-	ref: string;
+	/** The document reference to this resource in the database. */
+	ref: Reference;
 
-	/** Timestamp of when this resource was created. */
+	/** The timestamp of when this resource was created. */
 	ts: number;
 
-	/** Data contained within this resource. */
+	/** The data contained within this resource. */
 	data: T;
 }
 
-export type { Document };
+/** Represents a document reference in the Fauna database. */
+interface Reference {
+	/** The value of this reference. */
+	value: {
+		/** The ID of this reference. */
+		id: string;
+		/** The collection in which the referenced document lies. */
+		collection?: Reference;
+	};
+}
+
+export type { Document, Reference };
