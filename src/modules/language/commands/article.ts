0dc60f8066c783dab01cc4065324185de8e2ceff
vxern <vxern@wordcollector.co.uk>
2022-06-15 14:08:08 +0100

    feat: Complete `create`, `edit` and `view` commands for articles.

--- src/modules/language/commands/article.ts
+++ src/modules/language/commands/article.ts
@@ -1,4 +1,5 @@
 import {
+	_,
 	AutocompleteInteraction,
 	Collector,
 	Interaction,
@@ -11,9 +12,16 @@ import { Availability } from '../../../commands/availability.ts';
 import { Command } from '../../../commands/command.ts';
 import { OptionType } from '../../../commands/option.ts';
 import configuration from '../../../configuration.ts';
-import { Article } from '../../../database/structs/article.ts';
-import { mention, MentionType, time } from '../../../formatting.ts';
-import { paginate, toModal } from '../../../utils.ts';
+import { ArticleChange } from '../../../database/structs/articles/article-change.ts';
+import {
+	Article,
+	getContributorReferences,
+	getMostRecentArticleContent,
+} from '../../../database/structs/articles/article.ts';
+import { Document } from '../../../database/structs/document.ts';
+import { User } from '../../../database/structs/users/user.ts';
+import { mention, MentionType } from '../../../formatting.ts';
+import { Form, paginate, toModal } from '../../../utils.ts';
 
 const command: Command = {
 	name: 'article',
@@ -22,6 +30,16 @@ const command: Command = {
 		name: 'create',
 		type: OptionType.SUB_COMMAND,
 		handle: create,
+	}, {
+		name: 'edit',
+		type: OptionType.SUB_COMMAND,
+		options: [{
+			name: 'title',
+			type: OptionType.STRING,
+			required: true,
+			autocomplete: true,
+		}],
+		handle: edit,
 	}, {
 		name: 'view',
 		type: OptionType.SUB_COMMAND,
@@ -41,6 +59,42 @@ const command: Command = {
 
 /** Allows the user to write and submit an article. */
 async function create(client: Client, interaction: Interaction): Promise<void> {
+	const user = await client.database.getOrCreateUser('id', interaction.user.id);
+
+	if (!user) return;
+
+	const articles = await client.database.getArticles('author', user.ref);
+
+	if (!articles) return;
+
+	const articleTimestamps = articles
+		.map((document) => document.ts)
+		.sort((a, b) => b - a); // From most recent to least recent.
+
+	const timestampSlice = articleTimestamps.slice(
+		0,
+		configuration.guilds.articles.create.maximum,
+	);
+
+	const canCreateArticle =
+		timestampSlice.length < configuration.guilds.articles.create.maximum ||
+		timestampSlice.some((timestamp) =>
+			(Date.now() - timestamp) >= configuration.guilds.articles.create.interval
+		);
+
+	if (!canCreateArticle) {
+		interaction.respond({
+			type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+			ephemeral: true,
+			embeds: [{
+				title: 'Maximum number of articles reached',
+				description: `You must wait before submitting another article.`,
+				color: configuration.responses.colors.red,
+			}],
+		});
+		return;
+	}
+
 	interaction.showModal(toModal(configuration.forms.article));
 
 	const collector = new Collector({
@@ -67,23 +121,48 @@ async function create(client: Client, interaction: Interaction): Promise<void> {
 	const data = submission.data! as InteractionModalSubmitData;
 	const components = data.components;
 
-	const author = submission.user!;
+	function showArticleSubmissionFailure(): void {
+		submission.respond({
+			type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+			ephemeral: true,
+			embeds: [{
+				title: 'Failed to submit article',
+				description: `Your article failed to be submitted.`,
+				color: configuration.responses.colors.red,
+			}],
+		});
+	}
+
+	const author = await client.database.getOrCreateUser(
+		'id',
+		submission.user.id,
+	);
+
+	if (!author) {
+		return showArticleSubmissionFailure();
+	}
+
+	const footer = components[2]?.components[0]?.value;
 
 	const article: Article = {
-		title: components[0]!.components[0]!.value!,
-		author: author.id,
+		author: author.ref,
 		language: client.getLanguage(submission.guild!),
-		contributors: [author.id],
-		body: components[1]!.components[0]!.value!,
-		footer: components[2]?.components[0]?.value,
-		createdAt: Date.now(),
+		content: {
+			title: components[0]!.components[0]!.value!,
+			body: components[1]!.components[0]!.value!,
+			footer: (footer && footer.length !== 0) ? footer : undefined,
+		},
 	};
 
-	await client.database.createArticle(article);
+	const document = await client.database.createArticle(article);
+
+	if (!document) {
+		return showArticleSubmissionFailure();
+	}
 
 	client.logging.get(interaction.guild!.id)?.log(
 		'articleSubmit',
-		article,
+		document.data,
 		submission.member!,
 	);
 
@@ -93,17 +172,22 @@ async function create(client: Client, interaction: Interaction): Promise<void> {
 		embeds: [{
 			title: 'Article submitted successfully!',
 			description:
-				`Your article, \`${article.title}\`, has been submitted successfully.`,
+				`Your article, \`${document.data.content.title}\`, has been submitted successfully.`,
 			color: configuration.responses.colors.green,
 		}],
 	});
 }
 
-/** Displays an available article to the user. */
-async function view(client: Client, interaction: Interaction): Promise<void> {
+/** Allows the user to edit an existing article. */
+async function edit(client: Client, interaction: Interaction): Promise<void> {
 	const language = client.getLanguage(interaction.guild!);
+	const documents = await client.database.getArticles('language', language);
 
-	const articles = await client.database.getArticlesByLanguage(language);
+	if (!documents) {
+		return;
+	}
+
+	const articles = documents.map((document) => document.data);
 
 	if (interaction.isAutocomplete()) {
 		return showResults({
@@ -112,15 +196,240 @@ async function view(client: Client, interaction: Interaction): Promise<void> {
 		});
 	}
 
+	const user = await client.database.getOrCreateUser('id', interaction.user.id);
+
+	if (!user) return;
+
+	const articleChanges = await client.database.getArticleChanges(
+		'author',
+		user.ref,
+	);
+
+	if (!articleChanges) return;
+
+	const articleTimestamps = articleChanges
+		.map((document) => document.ts)
+		.sort((a, b) => b - a); // From most recent to least recent.
+
+	const timestampSlice = articleTimestamps.slice(
+		0,
+		configuration.guilds.articles.edit.maximum,
+	);
+
+	const canCreateArticleChange =
+		timestampSlice.length < configuration.guilds.articles.edit.maximum ||
+		timestampSlice.some((timestamp) =>
+			(Date.now() - timestamp) >= configuration.guilds.articles.edit.interval
+		);
+
+	if (!canCreateArticleChange) {
+		interaction.respond({
+			type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+			ephemeral: true,
+			embeds: [{
+				title: 'Maximum number of edits reached',
+				description: `You must wait before trying to edit another article.`,
+				color: configuration.responses.colors.red,
+			}],
+		});
+		return;
+	}
+
 	const data = interaction.data as InteractionApplicationCommandData;
 	const index = parseInt(data.options[0]!.options![0]!.value!);
-	const show = data.options[0]!.options!.find((option) => option.name === 'show')?.value ??
-		false;
-	const article = articles[index]! as Article;
+
+	const document = documents[index]!;
+	const changes = await client.database.getArticleChanges(
+		'articleReference',
+		document.ref,
+	);
+
+	if (!changes) {
+		return;
+	}
+
+	const content = getMostRecentArticleContent({
+		article: document.data,
+		changes: changes,
+	});
+
+	const prefilledForm = _.merge(
+		configuration.forms.article,
+		{
+			fields: Object.fromEntries(
+				Object.entries(content).map(([field, value]) => {
+					return [field, { value: value }];
+				}),
+			),
+		},
+	) as Form;
+
+	interaction.showModal(toModal(prefilledForm));
+
+	const collector = new Collector({
+		event: 'interactionCreate',
+		client: interaction.client,
+		filter: (selection: Interaction) => {
+			if (!selection.isModalSubmit()) return false;
+
+			if (selection.user.id !== interaction.user.id) return false;
+
+			if (selection.data.custom_id !== 'article_editor') return false;
+
+			return true;
+		},
+		deinitOnEnd: true,
+	});
+
+	collector.collect();
+
+	const submission = (await collector.waitFor('collect'))[0] as Interaction;
+
+	collector.end();
+
+	function showArticleEditFailure(): void {
+		submission.respond({
+			type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+			ephemeral: true,
+			embeds: [{
+				title: 'Failed to update article',
+				description: `Failed to edit article..`,
+				color: configuration.responses.colors.red,
+			}],
+		});
+	}
+
+	if (!user) {
+		return showArticleEditFailure();
+	}
+
+	const modalData = submission.data! as InteractionModalSubmitData;
+	const components = modalData.components;
+
+	const change = await client.database.changeArticle({
+		author: user.ref,
+		article: document.ref,
+		content: {
+			title: components[0]!.components[0]!.value!,
+			body: components[1]!.components[0]!.value!,
+			footer: components[2]!.components[0]!.value,
+		},
+	});
+
+	if (!change) {
+		return showArticleEditFailure();
+	}
+
+	client.logging.get(interaction.guild!.id)?.log(
+		'articleEdit',
+		document.data,
+		change.data,
+		submission.member!,
+	);
+
+	submission.respond({
+		type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+		ephemeral: true,
+		embeds: [{
+			title: 'Article edited successfully!',
+			description: `Your edit was saved.`,
+			color: configuration.responses.colors.green,
+		}],
+	});
+}
+
+/** Displays an available article to the user. */
+async function view(client: Client, interaction: Interaction): Promise<void> {
+	const language = client.getLanguage(interaction.guild!);
+	const documentsUnprocessed = await client.database.getArticles(
+		'language',
+		language,
+	);
+
+	if (!documentsUnprocessed) {
+		return;
+	}
+
+	if (interaction.isAutocomplete()) {
+		const articles = documentsUnprocessed.map((document) => document.data);
+
+		return showResults({
+			interaction: interaction,
+			articles: articles,
+		});
+	}
+
+	const documentsChanges = await Promise.all(
+		documentsUnprocessed.map((document) =>
+			new Promise<[Document<Article>, Document<ArticleChange>[] | undefined]>((
+				resolve,
+			) =>
+				client.database.getArticleChanges(
+					'articleReference',
+					document.ref,
+				)
+					.then((changes) => {
+						resolve([document, changes]);
+					})
+			)
+		),
+	);
+
+	if (
+		documentsChanges.map(([_document, change]) => change).includes(undefined)
+	) {
+		return;
+	}
+
+	const documents = documentsChanges.map(([document, changes]) => {
+		document.data.content = getMostRecentArticleContent({
+			article: document.data,
+			changes: changes!,
+		});
+
+		return document;
+	});
+
+	const data = interaction.data as InteractionApplicationCommandData;
+	const index = parseInt(data.options[0]!.options![0]!.value!);
+	const show =
+		data.options[0]!.options!.find((option) => option.name === 'show')?.value ??
+			false;
+	const document = documents[index];
+
+	if (!document) {
+		return;
+	}
+
+	const changes = await client.database.getArticleChanges(
+		'articleReference',
+		document.ref,
+	);
+
+	if (!changes) {
+		return;
+	}
+
+	const contributorReferences = getContributorReferences({
+		article: document.data,
+		changes: changes,
+	});
+
+	const contributors = await Promise.all(
+		contributorReferences.map((reference) =>
+			client.database.getOrCreateUser('reference', reference)
+		),
+	);
+
+	if (contributors.includes(undefined)) {
+		return;
+	}
 
 	return showArticle({
 		interaction: interaction,
-		article: article,
+		document: document,
+		changes: changes,
+		contributors: contributors as Document<User>[],
 		show: show,
 	});
 }
@@ -137,14 +446,14 @@ function showResults(
 
 	const value = argument.value as string;
 	const articlesByName = articles.filter((article) =>
-		article.title.toLowerCase().includes(value.toLowerCase())
+		article.content.title.toLowerCase().includes(value.toLowerCase())
 	);
 
 	interaction.respond({
 		type: InteractionResponseType.APPLICATION_COMMAND_AUTOCOMPLETE_RESULT,
 		choices: articlesByName.map((article, index) => {
 			return {
-				name: article.title,
+				name: article.content.title,
 				value: index.toString(),
 			};
 		}),
@@ -153,26 +462,31 @@ function showResults(
 
 function showArticle({
 	interaction,
-	article,
+	document,
+	changes,
+	contributors,
 	show,
 }: {
 	interaction: Interaction;
-	article: Article;
+	document: Document<Article>;
+	changes: Document<ArticleChange>[];
+	contributors: Document<User>[];
 	show: boolean;
 }): void {
-	const contributorsString = article.contributors.map((id) =>
-		mention(id, MentionType.USER)
+	const contributorsString = contributors.map((document) =>
+		mention(document.data.account.id, MentionType.USER)
 	).join(', ');
 
-	const sections = article.body.split('\n\n');
+	const content = getMostRecentArticleContent({
+		article: document.data,
+		changes: changes,
+	});
+
+	const sections = content.body.split('\n\n');
 
 	const pages = sections.reduce<string[]>(
 		(accumulated, section) => {
-			if (accumulated.length === 0) {
-				accumulated.push('');
-			}
-
-			const last = accumulated[accumulated.length - 1]!;
+			const last = accumulated[accumulated.length - 1] ?? '';
 
 			const pendingLength = last.length + section.length;
 			const maximumLength = 1024 - 2 * (accumulated.length - 1);
@@ -180,7 +494,11 @@ function showArticle({
 			if (
 				pendingLength < maximumLength && section[section.length - 1] !== ':'
 			) {
-				accumulated[accumulated.length - 1] = `${last}\n\n${section}`;
+				const lastIndex = Math.max(0, accumulated.length - 1);
+
+				accumulated[lastIndex] = last.length === 0
+					? section
+					: `${last}\n\n${section}`;
 			} else {
 				accumulated.push(section);
 			}
@@ -190,19 +508,13 @@ function showArticle({
 		[],
 	);
 
-	const lastUpdated = (!article.changes || article.changes.length === 0)
-		? article.createdAt!
-		: article.changes[article.changes.length - 1]!.createdAt;
-
 	paginate({
 		interaction: interaction,
 		elements: pages,
 		embed: {
-			title: article.title,
+			title: content.title,
 			description: `
-Contributors: ${contributorsString}
-Created: ${time(article.createdAt!)}
-Last updated: ${time(lastUpdated)}.`,
+Contributors: ${contributorsString}`,
 			color: configuration.responses.colors.blue,
 		},
 		view: {
