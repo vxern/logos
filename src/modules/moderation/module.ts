cbcc1c4e15bfeea0ef88e2b8a00f4df358a993bc
vxern <vxern@wordcollector.co.uk>
2022-06-25 21:08:38 +0100

    fix: Remove inexistent commands.

--- src/modules/moderation/module.ts
+++ src/modules/moderation/module.ts
@@ -3,25 +3,19 @@ import ban from './commands/ban.ts';
 import cite from './commands/cite.ts';
 import kick from './commands/kick.ts';
 import list from './commands/list.ts';
-import mute from './commands/mute.ts';
 import pardon from './commands/pardon.ts';
 import purge from './commands/purge.ts';
 import reprimand from './commands/reprimand.ts';
 import slowmode from './commands/slowmode.ts';
-import unban from './commands/unban.ts';
-import unmute from './commands/unmute.ts';
 
 const commands: Record<string, Command> = {
 	ban,
 	cite,
 	kick,
 	list,
-	mute,
 	pardon,
 	purge,
 	slowmode,
-	unban,
-	unmute,
 	reprimand,
 };
 
