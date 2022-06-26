8130d70eb49ccc06a29bacac0eb2c6a00c0665e7
vxern <vxern@wordcollector.co.uk>
2022-06-26 23:17:34 +0100

    feat: Implement `/volume` command.

--- src/modules/music/commands/volume.ts
+++ src/modules/music/commands/volume.ts
@@ -1,4 +1,9 @@
-import { ApplicationCommandOptionType } from '../../../../deps.ts';
+import {
+	ApplicationCommandInteraction,
+	ApplicationCommandOptionType,
+	Interaction,
+} from '../../../../deps.ts';
+import { Client } from '../../../client.ts';
 import { Availability } from '../../../commands/structs/availability.ts';
 import { Command } from '../../../commands/structs/command.ts';
 import configuration from '../../../configuration.ts';
@@ -10,10 +15,17 @@ const command: Command = {
 		name: 'display',
 		description: 'Displays the volume of playback.',
 		type: ApplicationCommandOptionType.SUB_COMMAND,
+		handle: display,
+		options: [{
+			name: 'show',
+			description: 'If set to true, the queue view will be shown to others.',
+			type: ApplicationCommandOptionType.BOOLEAN,
+		}],
 	}, {
 		name: 'set',
 		description: 'Sets the volume of playback.',
 		type: ApplicationCommandOptionType.SUB_COMMAND,
+		handle: set,
 		options: [{
 			name: 'volume',
 			description: `A value between 0 and ${configuration.music.maxima.volume}`,
@@ -22,5 +34,52 @@ const command: Command = {
 		}],
 	}],
 };
+
+function display(
+	client: Client,
+	interaction: Interaction,
+): void {
+	const controller = client.music.get(interaction.guild!.id)!;
+
+	const show =
+		(<ApplicationCommandInteraction> interaction).data.options[0]!.options![0]
+			?.value ??
+			false;
+
+	interaction.respond({
+		ephemeral: !show,
+		embeds: [{
+			title: '🔊 Volume',
+			description: `The current volume is ${controller.volume}%.`,
+			color: configuration.interactions.responses.colors.invisible,
+		}],
+	});
+}
+
+function set(
+	client: Client,
+	interaction: Interaction,
+): void {
+	const controller = client.music.get(interaction.guild!.id)!;
+
+	const volume = Number(
+		(<ApplicationCommandInteraction> interaction).data.options[0]!
+			.value! as string,
+	);
+
+	if (volume < 0 || volume > configuration.music.maxima.volume) {
+		interaction.respond({
+			embeds: [{
+				title: 'Invalid volume',
+				description:
+					`Song volume may not be negative, and it may not be bigger than ${configuration.music.maxima.volume}%.`,
+				color: configuration.interactions.responses.colors.red,
+			}],
+		});
+		return;
+	}
+
+	controller.setVolume(interaction, volume);
+}
 
 export default command;
