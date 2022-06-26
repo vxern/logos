c57a0c71c3581ab87d7ed7c3b1ffee1d5cbd5cf5
vxern <vxern@wordcollector.co.uk>
2022-06-26 23:17:27 +0100

    feat: Implement `/history` command.

--- src/modules/music/commands/history.ts
+++ src/modules/music/commands/history.ts
@@ -1,21 +1,43 @@
-import { Interaction } from '../../../../deps.ts';
+import {
+	ApplicationCommandInteraction,
+	ApplicationCommandOptionType,
+	Interaction,
+} from '../../../../deps.ts';
 import { Client } from '../../../client.ts';
 import { Availability } from '../../../commands/structs/availability.ts';
 import { Command } from '../../../commands/structs/command.ts';
+import { displayListings } from '../module.ts';
 
 const command: Command = {
 	name: 'history',
 	availability: Availability.MEMBERS,
 	description: 'Displays a list of previously played songs.',
 	handle: history,
+	options: [{
+		name: 'show',
+		description: 'If set to true, the queue view will be shown to others.',
+		type: ApplicationCommandOptionType.BOOLEAN,
+	}],
 };
 
-async function history(
-	_client: Client,
-	_interaction: Interaction,
-): Promise<void> {
-	/// TODO(vxern):
-	/// Display list of played songs in a browsable list.
+function history(
+	client: Client,
+	interaction: Interaction,
+): void {
+	const controller = client.music.get(interaction.guild!.id)!;
+
+	const show =
+		(<ApplicationCommandInteraction> interaction).data.options?.find((
+			option,
+		) => option.name === 'show')?.value ??
+			false;
+
+	displayListings({
+		interaction: interaction,
+		title: '📋 History',
+		listings: controller.history,
+		show: show,
+	});
 }
 
 export default command;
