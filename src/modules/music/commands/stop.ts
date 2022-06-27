0e045aeab06ad5e0635cf124916729744dfd4b97
vxern <vxern@wordcollector.co.uk>
2022-06-27 18:29:56 +0100

    feat: Add `/stop` command.

---
+++ src/modules/music/commands/stop.ts
@@ -0,0 +1,36 @@
+import { Interaction } from '../../../../deps.ts';
+import { Client } from '../../../client.ts';
+import { Availability } from '../../../commands/structs/availability.ts';
+import { Command } from '../../../commands/structs/command.ts';
+import configuration from '../../../configuration.ts';
+
+const command: Command = {
+	name: 'stop',
+	availability: Availability.MEMBERS,
+	description:
+		'Stops the current listening session, clearing the queue and song history.',
+	handle: stop,
+};
+
+async function stop(client: Client, interaction: Interaction): Promise<void> {
+	const controller = client.music.get(interaction.guild!.id)!;
+
+	const [canAct, _] = await controller.verifyMemberVoiceState(interaction);
+	if (!canAct) return;
+
+	if (!controller.isOccupied) {
+		interaction.respond({
+			ephemeral: true,
+			embeds: [{
+				title: 'Nothing to stop',
+				description: 'There is no active listening session.',
+				color: configuration.interactions.responses.colors.yellow,
+			}],
+		});
+		return;
+	}
+
+	controller.reset();
+}
+
+export default command;
