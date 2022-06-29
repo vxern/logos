b86e2c53415b8eeb758d497df966556499379988
vxern <vxern@wordcollector.co.uk>
2022-06-29 16:01:35 +0100

    feat: Add custom regex for checking YouTube links. Added playlist parsing.

--- src/modules/music/data/sources/youtube.ts
+++ src/modules/music/data/sources/youtube.ts
@@ -3,37 +3,59 @@ import configuration from '../../../../configuration.ts';
 import { SongListing } from '../song-listing.ts';
 import { ListingResolver } from './sources.ts';
 
+const videoExpression = new RegExp(
+	/^(?:https?:)?(?:\/\/)?(?:youtu\.be\/|(?:www\.|m\.)?youtube\.com\/(?:watch|v|embed)(?:\.php)?(?:\?.*v=|\/))([a-zA-Z0-9\_-]{7,15})(?:[\?&][a-zA-Z0-9\_-]+=[a-zA-Z0-9\_-]+)*$/,
+);
+
 const resolver: ListingResolver = async (interaction, option) => {
-	if (option.name === 'url') {
-		if (!YouTube.validate(option.value, 'VIDEO')) {
-			interaction.respond({
-				embeds: [{
-					title: 'The URL is invalid.',
-					description:
-						'The provided link is not of a valid YouTube URL format.',
-					color: configuration.interactions.responses.colors.red,
-				}],
-				ephemeral: true,
-			});
-			return undefined;
-		}
+	if (option.name !== 'url') {
+		const videos = await YouTube.search(option.value, {
+			limit: 10,
+			type: 'video',
+			safeSearch: true,
+		});
 
-		const video = await YouTube.getVideo(option.value);
+		if (videos.length === 0) return undefined;
 
-		return fromYouTubeVideo(video, interaction.user.id);
+		// TODO(vxern): Allow the user to pick the specific video using a selection menu.
+
+		return fromYouTubeVideo(videos[0]!, interaction.user.id);
 	}
 
-	const videos = await YouTube.search(option.value, {
-		limit: 10,
-		type: 'video',
-		safeSearch: true,
-	});
+	if (!videoExpression.exec(option.value)) {
+		interaction.respond({
+			embeds: [{
+				title: 'The URL is invalid',
+				description: 'The provided link is not of a valid YouTube URL format.',
+				color: configuration.interactions.responses.colors.red,
+			}],
+			ephemeral: true,
+		});
+		return undefined;
+	}
 
-	if (videos.length === 0) return undefined;
+	if (option.value!.includes('list')) {
+		const playlist = await YouTube.getPlaylist(option.value);
 
-	// TODO(vxern): Allow the user to pick the specific video using a selection menu.
+		return {
+			type: 'SONG_COLLECTION',
+			source: 'YouTube',
+			requestedBy: interaction.user.id,
+			content: {
+				title: playlist.title!,
+				songs: playlist.videos.map((video) => ({
+					title: video.title!,
+					url: video.url!,
+					duration: video.duration,
+				})),
+				position: -1,
+			},
+		};
+	}
 
-	return fromYouTubeVideo(videos[0]!, interaction.user.id);
+	const video = await YouTube.getVideo(option.value);
+
+	return fromYouTubeVideo(video, interaction.user.id);
 };
 
 /**
