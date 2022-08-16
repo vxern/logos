8f0e0108229c74d7dfd0e69b46a167998d516db3
vxern <vxern@wordcollector.co.uk>
2022-08-16 21:52:12 +0100

    misc: Migrate code from `harmony` to `discordeno`. Extract links into new `constants.ts` file.

---
+++ src/constants.ts
@@ -0,0 +1,13 @@
+const links = {
+	typescriptWebsite: 'https://www.typescriptlang.org/',
+	denoWebsite: 'https://deno.land/',
+	discordApiWebsite: 'https://discord.com/developers/docs/intro',
+	discordenoRepository: 'https://github.com/discordeno/discordeno',
+
+	learnArmenianListingWebsite: 'https://discord.me/learnarmenian',
+	learnRomanianListingWebsite: 'https://discord.me/learnromanian',
+
+	talonRepositoryLink: 'https://github.com/linguition/talon',
+};
+
+export { links };
--- src/modules/information/commands/information/bot.ts
+++ src/modules/information/commands/information/bot.ts
@@ -1,50 +1,70 @@
-import { Interaction } from '../../../../../deps.ts';
-import { Client } from '../../../../client.ts';
+import {
+	ApplicationCommandFlags,
+	avatarURL,
+	Interaction,
+	InteractionResponseTypes,
+	sendInteractionResponse,
+} from '../../../../../deps.ts';
+import { Client, getBotUser } from '../../../../client.ts';
 import configuration from '../../../../configuration.ts';
+import { links } from '../../../../constants.ts';
 import { list } from '../../../../formatting.ts';
 
 /** Displays information about the bot (application). */
-function displayBotInformation(
-	_client: Client,
+async function displayBotInformation(
+	client: Client,
 	interaction: Interaction,
-): void {
-	const botUser = interaction.client.user!;
+): Promise<unknown> {
+	const botUser = await getBotUser(client);
+	if (!botUser) return;
 
-	interaction.respond({
-		ephemeral: true,
-		embeds: [{
-			title: botUser.username,
-			thumbnail: { url: botUser.avatarURL() },
-			color: configuration.interactions.responses.colors.invisible,
-			fields: [{
-				name: 'What am I?',
-				value:
-					`I am **${botUser.username}**, a Discord application created to provide language-learning servers with the highest quality features, such as:
-${
-						list([
-							'Rich social interactions',
-							'Intuitive role management',
-							'Translation and morphology look-ups',
-							'Music playback',
-							'Article creation',
-							'Server structure synchronisation',
-						])
-					}`,
-			}, {
-				name: 'How was I made?',
-				value:
-					'I am powered by [TypeScript](https://www.typescriptlang.org/) running within [Deno](https://deno.land/). I interact with [Discord\'s API](https://discord.com/developers/docs/intro) with the help of [Harmony](https://github.com/harmonyland/harmony).',
-			}, {
-				name: 'How can you add me to your server?',
-				value:
-					'You cannot just yet. I was made for the purpose of managing a select few language-learning servers, such as the [Armenian](https://discord.me/learnarmenian), [Belarusian](https://discord.me/learnbelarusian) and [Romanian](https://discord.me/learnromanian) communities.',
-			}, {
-				name: 'Am I open-source?',
-				value:
-					'Unfortunately, no. However, my predecessor, Talon, *is*. You can view his source code [here](https://github.com/linguition/talon).',
-			}],
-		}],
-	});
+	return sendInteractionResponse(
+		client.bot,
+		interaction.id,
+		interaction.token,
+		{
+			type: InteractionResponseTypes.ChannelMessageWithSource,
+			data: {
+				flags: ApplicationCommandFlags.Ephemeral,
+				embeds: [{
+					title: botUser.username,
+					thumbnail: {
+						url: avatarURL(client.bot, client.bot.id, botUser.discriminator, {
+							avatar: botUser.avatar,
+						}),
+					},
+					color: configuration.interactions.responses.colors.invisible,
+					fields: [{
+						name: 'What am I?',
+						value:
+							`I am **${botUser.username}**, an application created to provide language-learning Discord communities with the highest quality features, such as:
+  ${
+								list([
+									'Rich social interactions',
+									'Intuitive role management',
+									'Translation and morphology look-ups',
+									'Music playback',
+									'Article creation',
+									'Server structure synchronisation',
+								])
+							}`,
+					}, {
+						name: 'How was I made?',
+						value:
+							`I am powered by [TypeScript](${links.typescriptWebsite}) running within [Deno](${links.denoWebsite}). I interact with [Discord\'s API](${links.discordApiWebsite}) with the help of [discordeno](${links.discordenoRepository}).`,
+					}, {
+						name: 'How can you add me to your server?',
+						value:
+							`You cannot just yet. I was made for the purpose of managing a select few language-learning communities, such as [Learn Armenian](${links.learnArmenianListingWebsite}) and [Learn Romanian](${links.learnRomanianListingWebsite}).`,
+					}, {
+						name: 'Am I open-source?',
+						value:
+							`Unfortunately, no. However, my predecessor, Talon, *is*. You can view his source code [here](${links.talonRepositoryLink}).`,
+					}],
+				}],
+			},
+		},
+	);
 }
 
 export { displayBotInformation };
