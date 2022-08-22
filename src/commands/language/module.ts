cbbaf2b40e95d63963fc959a51cb6fe972c80765
vxern <vxern@wordcollector.co.uk>
2022-08-22 22:06:28 +0100

    misc: Migrate from `harmony` to `discordeno`. Move file to `/commands` subdirectory.

---
+++ src/commands/language/module.ts
@@ -0,0 +1,79 @@
+import { parse as parseCSV } from 'https://deno.land/std@0.127.0/encoding/csv.ts';
+import { Client } from '../../client.ts';
+import game from './commands/game.ts';
+import resources from './commands/resources.ts';
+import word from './commands/word.ts';
+import translate from './commands/translate.ts';
+import article from './commands/article.ts';
+import { DictionaryAdapter, DictionaryScopes } from './data/dictionary.ts';
+import dexonline from './data/dictionaries/dexonline.ts';
+import dictionarDeSinonime from './data/dictionaries/dictionar-de-antonime.ts';
+import dictionarDeAntonime from './data/dictionaries/dictionar-de-sinonime.ts';
+import { SentencePair } from './data/sentence.ts';
+
+const commands = [article, game, resources, translate, word];
+
+const dictionaryAdapters = [
+	dexonline,
+	dictionarDeAntonime,
+	dictionarDeSinonime,
+];
+
+const dictionaryAdapterLists: Record<string, DictionaryAdapter[]> = {};
+const sentenceLists: Record<string, SentencePair[]> = {};
+
+/** Loads dictionary adapters and sentence lists. */
+async function loadComponents(client: Client): Promise<void> {
+	for (const language of client.languages.values()) {
+		if (language in dictionaryAdapterLists) continue;
+
+		dictionaryAdapterLists[language] = [];
+	}
+
+	for (const dictionaryAdapter of dictionaryAdapters) {
+		if (dictionaryAdapter.scope === DictionaryScopes.Omnilingual) {
+			for (const language of Object.keys(dictionaryAdapterLists)) {
+				dictionaryAdapterLists[language]!.push(dictionaryAdapter);
+			}
+			continue;
+		}
+
+		for (const language of dictionaryAdapter.languages!) {
+			// Ignore unsupported languages.
+			if (!(language in dictionaryAdapterLists)) continue;
+
+			dictionaryAdapterLists[language]!.push(dictionaryAdapter);
+		}
+	}
+
+	for await (
+		const entry of Deno.readDir('./src/modules/language/data/sentences')
+	) {
+		if (!entry.isFile) continue;
+
+		const language = entry.name.split('.')[0]!;
+
+		const records = <[string, string, string, string][]> await parseCSV(
+			await Deno.readTextFile(
+				`./src/modules/language/data/sentences/${entry.name}`,
+			),
+			{
+				lazyQuotes: true,
+				separator: '\t',
+			},
+		);
+
+		sentenceLists[language] = [];
+		for (
+			const [_sentenceID, sentence, _translationID, translation] of records
+		) {
+			sentenceLists[language]!.push({
+				sentence: sentence!,
+				translation: translation!,
+			});
+		}
+	}
+}
+
+export { dictionaryAdapterLists, loadComponents, sentenceLists };
+export default commands;
