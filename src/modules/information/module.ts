a5623733f8f65a4ce2154172aa6060aa49387659
vxern <vxern@wordcollector.co.uk>
2022-08-22 21:13:48 +0100

    misc: Move `module.ts` to `/commands` subdirectory.

--- src/modules/information/module.ts
+++
@@ -1,8 +0,0 @@
-import { Command } from '../../commands/structs/command.ts';
-import information from './commands/information.ts';
-
-const commands: Record<string, Command> = {
-	information,
-};
-
-export default commands;
