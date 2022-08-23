ff92d771080d578495367feb835f3ef977b8c03e
vxern <vxern@wordcollector.co.uk>
2022-08-23 15:06:02 +0100

    fix: Adapt code and fix YouTube query resolver.

---
+++ src/commands/music/data/sources/youtube.ts
@@ -0,0 +1,170 @@
+import {
+	Playlist,
+	Video,
+	YouTube,
+} from 'https://deno.land/x/youtube_sr@v4.1.17/mod.ts';
+import {
+	ApplicationCommandFlags,
+	Interaction,
+	InteractionResponseTypes,
+	InteractionTypes,
+	MessageComponentTypes,
+	sendInteractionResponse,
+} from '../../../../../deps.ts';
+import { Client } from '../../../../client.ts';
+import configuration from '../../../../configuration.ts';
+import { createInteractionCollector, trim } from '../../../../utils.ts';
+import { SongListing, SongListingContentTypes } from '../song-listing.ts';
+import { ListingResolver } from './sources.ts';
+
+const urlExpression = new RegExp(
+	/^(?:https?:)?(?:\/\/)?(?:youtu\.be\/|(?:www\.|m\.)?youtube\.com\/(?:watch|v|embed)(?:\.php)?(?:\?.*v=|\/))([a-zA-Z0-9\_-]{7,15})(?:[\?&][a-zA-Z0-9\_-]+=[a-zA-Z0-9\_-]+)*$/,
+);
+
+const resolver: ListingResolver = async (client, interaction, query) => {
+	const urlExpressionExecuted = urlExpression.exec(query);
+	if (!urlExpressionExecuted) {
+		return search(client, interaction, query);
+	}
+
+	const url = urlExpressionExecuted[0]!;
+
+	if (url.includes('&list=')) {
+		const playlist = await YouTube.getPlaylist(query);
+
+		return fromYouTubePlaylist(playlist, interaction.user.id);
+	}
+
+	const video = await YouTube.getVideo(query);
+
+	return fromYouTubeVideo(video, interaction.user.id);
+};
+
+async function search(
+	client: Client,
+	interaction: Interaction,
+	query: string,
+): Promise<SongListing | undefined> {
+	const results = <(Video | Playlist)[]> await YouTube.search(query, {
+		limit: 20,
+		type: 'all',
+		safeSearch: false,
+	}).then((result) =>
+		result.filter((element) =>
+			element.type === 'video' || element.type === 'playlist'
+		)
+	);
+	if (results.length === 0) return undefined;
+
+	return new Promise<SongListing | undefined>((resolve) => {
+		const customId = createInteractionCollector(
+			client,
+			{
+				type: InteractionTypes.MessageComponent,
+				userId: interaction.user.id,
+				limit: 1,
+				onCollect: async (bot, selection) => {
+					sendInteractionResponse(bot, selection.id, selection.token, {
+						type: InteractionResponseTypes.DeferredUpdateMessage,
+					});
+
+					const indexString = <string | undefined> selection.data?.values?.at(
+						0,
+					);
+					if (!indexString) return resolve(undefined);
+
+					const index = Number(indexString);
+					if (isNaN(index)) return resolve(undefined);
+
+					const result = results[index]!;
+					if (result.type === 'video') {
+						return resolve(fromYouTubeVideo(result, interaction.user.id));
+					}
+
+					const playlist = await YouTube.getPlaylist(result.url!);
+					return resolve(fromYouTubePlaylist(playlist, interaction.user.id));
+				},
+			},
+		);
+
+		sendInteractionResponse(client.bot, interaction.id, interaction.token, {
+			type: InteractionResponseTypes.ChannelMessageWithSource,
+			data: {
+				flags: ApplicationCommandFlags.Ephemeral,
+				embeds: [{
+					title: 'Select a song / song collection',
+					description:
+						'Select a song or song collection from the choices below.',
+					color: configuration.interactions.responses.colors.blue,
+				}],
+				components: [{
+					type: MessageComponentTypes.ActionRow,
+					components: [{
+						type: MessageComponentTypes.SelectMenu,
+						customId: customId,
+						minValues: 1,
+						maxValues: 1,
+						options: results.map((result, index) => ({
+							emoji: {
+								name: result.type === 'video'
+									? configuration.music.symbols.song
+									: configuration.music.symbols.collection,
+							},
+							label: trim(result.title!, 100),
+							value: index.toString(),
+						})),
+					}],
+				}],
+			},
+		});
+	});
+}
+
+/**
+ * Creates a song listing from a YouTube video.
+ */
+function fromYouTubeVideo(
+	video: Video,
+	requestedBy: bigint,
+): SongListing | undefined {
+	if (!video.id) return undefined;
+
+	return {
+		source: 'YouTube',
+		requestedBy: requestedBy,
+		content: {
+			type: SongListingContentTypes.Song,
+			title: video.title!,
+			url: video.url!,
+			duration: video.duration,
+		},
+	};
+}
+
+/**
+ * Creates a song listing from a YouTube playlist.
+ */
+function fromYouTubePlaylist(
+	playlist: Playlist,
+	requestedBy: bigint,
+): SongListing | undefined {
+	if (!playlist.id) return undefined;
+
+	return {
+		source: 'YouTube',
+		requestedBy: requestedBy,
+		content: {
+			type: SongListingContentTypes.Collection,
+			title: playlist.title!,
+			songs: playlist.videos.map((video) => ({
+				type: SongListingContentTypes.Song,
+				title: video.title!,
+				url: video.url!,
+				duration: video.duration,
+			})),
+			position: -1,
+		},
+	};
+}
+
+export default resolver;
