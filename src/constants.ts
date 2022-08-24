f07c1017e5a46f60f41d8f5dc382cf03be46663f
vxern <vxern@wordcollector.co.uk>
2022-08-24 14:49:24 +0100

    feat: Add deepL API links. Format.

--- src/constants.ts
+++ src/constants.ts
@@ -1,13 +1,19 @@
 const links = {
-	typescriptWebsite: 'https://www.typescriptlang.org/',
-	denoWebsite: 'https://deno.land/',
-	discordApiWebsite: 'https://discord.com/developers/docs/intro',
-	discordenoRepository: 'https://github.com/discordeno/discordeno',
+  typescriptWebsite: 'https://www.typescriptlang.org/',
+  denoWebsite: 'https://deno.land/',
+  discordApiWebsite: 'https://discord.com/developers/docs/intro',
+  discordenoRepository: 'https://github.com/discordeno/discordeno',
 
-	learnArmenianListingWebsite: 'https://discord.me/learnarmenian',
-	learnRomanianListingWebsite: 'https://discord.me/learnromanian',
+  learnArmenianListingWebsite: 'https://discord.me/learnarmenian',
+  learnRomanianListingWebsite: 'https://discord.me/learnromanian',
 
-	talonRepositoryLink: 'https://github.com/linguition/talon',
+  talonRepositoryLink: 'https://github.com/linguition/talon',
 };
 
-export { links };
+const deepLApiBaseLink = 'https://api-free.deepl.com/v2/';
+const deepLApiEndpoints = {
+	languages: `${deepLApiBaseLink}languages`,
+	translate: `${deepLApiBaseLink}translate`,
+};
+
+export { links, deepLApiEndpoints };
