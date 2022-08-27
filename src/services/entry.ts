59e6a02c2b1664d83f82785c33de4914a7aeed2a
vxern <vxern@wordcollector.co.uk>
2022-08-27 23:36:14 +0100

    misc: Migrate code from `harmony` to `discordeno`. Move file to under `/services`.

---
+++ src/services/entry.ts
@@ -0,0 +1,46 @@
+import { Interaction, InteractionTypes } from '../../deps.ts';
+import { Client } from '../client.ts';
+import { ServiceStarter } from './service.ts';
+import { createInteractionCollector } from '../utils.ts';
+import { onAcceptRules } from './entry/accept-rules.ts';
+import { onSelectLanguageProficiency } from './entry/select-language-proficiency.ts';
+
+const entrySteps = [
+	'ACCEPTED_RULES',
+	'SELECTED_LANGUAGE_PROFICIENCY',
+] as const;
+type Step = (typeof entrySteps)[number];
+
+type EntryInteractionHandler = (
+	client: Client,
+	interaction: Interaction,
+	parameter: string,
+) => Promise<void> | void;
+
+const interactionHandlers: { [key in Step]: EntryInteractionHandler } = {
+	'ACCEPTED_RULES': onAcceptRules,
+	'SELECTED_LANGUAGE_PROFICIENCY': onSelectLanguageProficiency,
+};
+
+const service: ServiceStarter = (client) => {
+	for (const entryStep of entrySteps) {
+		createInteractionCollector(client, {
+			type: InteractionTypes.MessageComponent,
+			customId: entryStep,
+			doesNotExpire: true,
+			onCollect: (_bot, interaction) => {
+				const selectionCustomId = interaction.data?.customId;
+				if (!selectionCustomId) return;
+
+				const stepAndParameter = selectionCustomId.split('|');
+				if (stepAndParameter.length !== 2) return;
+
+				const [step, parameter] = <[Step, string]> stepAndParameter;
+
+				interactionHandlers[step](client, interaction, parameter);
+			},
+		});
+	}
+};
+
+export default service;
