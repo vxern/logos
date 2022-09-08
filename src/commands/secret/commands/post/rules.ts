4797c02e23e6daa4e7398cfb0d91f58d2971325d
vxern <vxern@wordcollector.co.uk>
2022-09-08 12:35:40 +0100

    misc: Improve and simplify generation of rules. Fix the invite part of the rules.

--- src/commands/secret/commands/post/rules.ts
+++ src/commands/secret/commands/post/rules.ts
@@ -8,32 +8,19 @@ import {
 } from '../../../../../deps.ts';
 import { Client } from '../../../../client.ts';
 import configuration from '../../../../configuration.ts';
-import informationSections, {
-	InformationSection,
-} from '../../data/information/information-sections.ts';
+import informationSections from '../../data/information/information-sections.ts';
 
-function postRules(
+async function postRules(
 	client: Client,
 	interaction: Interaction,
-): void {
+): Promise<void> {
 	const guild = client.guilds.get(interaction.guildId!);
 	if (!guild) return;
 
 	const sections = Object.values(informationSections);
-
-	const sectionEmbedTuples = <[InformationSection, Embed][]> sections
-		.map<[InformationSection, Embed | undefined]>((
-			section,
-		) => [section, section.generateEmbed(client, guild)])
-		.filter(([_section, embed]) => embed);
-
-	const embeds = sectionEmbedTuples.map(([section, embed]) => {
-		// embed.title = name.toUpperCase();
-		embed.color = section.color;
-		// embed.image = { url: section.image };
-
-		return embed;
-	});
+	const embeds = <Embed[]> (await Promise.all(
+		sections.map((section) => section.generateEmbed(client, guild)),
+	)).filter((embed) => embed);
 
 	sendMessage(
 		client.bot,
