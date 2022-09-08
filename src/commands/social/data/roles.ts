aad349ae0d9f1983ff76435aa1df2063a7a72595
vxern <vxern@wordcollector.co.uk>
2022-09-08 13:12:32 +0100

    misc: Combine global and local role scopes into a single file `roles.ts`.

--- src/commands/social/data/roles.ts
+++ src/commands/social/data/roles.ts
@@ -1,9 +1,9 @@
-import { fromHex } from '../../../../utils.ts';
-import {
-	RoleCategory,
-	RoleCategoryTypes,
-} from '../structures/role-category.ts';
-import { RoleCollectionTypes } from '../structures/role-collection.ts';
+import configuration from '../../../configuration.ts';
+import { fromHex } from '../../../utils.ts';
+import { fromNames } from '../module.ts';
+import { Role } from './structures/role.ts';
+import { RoleCategory, RoleCategoryTypes } from './structures/role-category.ts';
+import { RoleCollectionTypes } from './structures/role-collection.ts';
 
 const categories: RoleCategory[] = [
 	{
@@ -143,6 +143,143 @@ const categories: RoleCategory[] = [
 			}],
 		},
 	},
+	{
+		name: 'Ethnicities',
+		description: 'Roles identifying one\'s ethnicity.',
+		type: RoleCategoryTypes.Category,
+		color: fromHex('#68d8d6'),
+		emoji: '🗾',
+		restrictToOneRole: false,
+		limit: 2,
+		collection: {
+			type: RoleCollectionTypes.CollectionLocalised,
+			onAssignMessage: (name) => `Your ethnicity is now ${name}.`,
+			onUnassignMessage: (name) => `Your ethnicity is no longer ${name}.`,
+			generateDescription: (name) => `I am of ${name} heritage.`,
+			lists: {
+				'Armenian': fromNames([
+					'Armeno-Tat',
+					'Circassian',
+					'Hemshen',
+					'Hidden',
+				]),
+				'Romanian': fromNames([
+					'Aromanian',
+					'Istro-Romanian',
+					'Megleno-Romanian',
+				]),
+			},
+		},
+	},
+	{
+		name: 'Dialects',
+		description:
+			'Roles specifying which dialect of the language one is learning.',
+		type: RoleCategoryTypes.Category,
+		color: fromHex('#00cc66'),
+		emoji: '🏷️',
+		restrictToOneRole: false,
+		collection: {
+			type: RoleCollectionTypes.CollectionLocalised,
+			onAssignMessage: (name) => `You are now learning ${name}.`,
+			onUnassignMessage: (name) => `You are no longer learning ${name}.`,
+			generateDescription: (name) => `I am learning ${name}.`,
+			lists: {
+				'Armenian': fromNames(
+					configuration.guilds.languages['Armenian'].dialects,
+				),
+			},
+		},
+	},
+	{
+		name: 'Regions',
+		description: 'Roles specifying which area of the country one is from.',
+		type: RoleCategoryTypes.Category,
+		color: fromHex('#c5e0d8'),
+		emoji: '🤷‍♂️',
+		restrictToOneRole: false,
+		limit: 2,
+		collection: {
+			type: RoleCollectionTypes.CollectionLocalised,
+			onAssignMessage: (name) => `You are now from ${name}.`,
+			onUnassignMessage: (name) => `You are no longer from ${name}.`,
+			generateDescription: (name) => `I am from ${name}.`,
+			lists: {
+				'Armenian': fromNames([
+					'Aragats\'otn / Արագածոտն',
+					'Ararat / Արարատ',
+					'Armavir / Արմավիր',
+					'Geghark\'unik\' / Գեղարքունիք',
+					'Kotayk\' / Կոտայք',
+					'Lorri / Լոռի',
+					'Shirak / Շիրակ',
+					'Syunik\' / Սյունիք',
+					'Tavush / Տավուշ',
+					'Vayots\' Dzor / Վայոց Ձոր',
+					'Yerevan / Երևան',
+				]),
+				'Belarusian': fromNames([
+					'Brest / Брэсцкая',
+					'Hrodna / Гродзенская',
+					'Homel / Гомельская',
+					'Mahilyow / Магілёўская',
+					'Minsk / Мінская',
+					'Vitsebsk / Вiцебская',
+				]),
+				'Romanian': fromNames([
+					'Banat',
+					'Basarabia',
+					'Bucovina',
+					'Crișana',
+					'Dobrogea',
+					'Maramureș',
+					'Moldova',
+					'Muntenia',
+					'Oltenia',
+					'Transilvania',
+				]),
+			},
+		},
+	},
 ];
 
-export default categories;
+/**
+ * Taking an array of categories with partial information filled in, completes
+ * the necessary information, and returns the complete role categories.
+ *
+ * @param roleCategories - The incomplete role categories.
+ * @returns The completed categories.
+ */
+function supplyMissingProperties(
+	roleCategories: RoleCategory[],
+): RoleCategory[] {
+	for (const category of roleCategories) {
+		if (category.type === RoleCategoryTypes.CategoryGroup) {
+			supplyMissingProperties(category.categories);
+			continue;
+		}
+
+		const collection = category.collection;
+
+		const roleLists =
+			collection.type === RoleCollectionTypes.CollectionLocalised
+				? Object.values<Role[]>(collection.lists)
+				: [collection.list];
+
+		for (const roleList of roleLists) {
+			for (const role of roleList) {
+				role.description ??= collection.generateDescription!(role.name);
+				role.onAssignMessage ??= collection.onAssignMessage;
+				if ('onUnassignMessage' in collection) {
+					role.onUnassignMessage = collection.onUnassignMessage;
+				}
+			}
+		}
+	}
+
+	return roleCategories;
+}
+
+const categoriesComplete = supplyMissingProperties(categories);
+
+export default categoriesComplete;
