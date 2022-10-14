f41f1bc54051ce9ea6d5192e537eff5659bfbf82
vxern <vxern@wordcollector.co.uk>
2022-10-14 16:40:46 +0100

    misc: Create `utils.ts` localisation file. Create `localise()` function.

---
+++ assets/localisations/utils.ts
@@ -0,0 +1,10 @@
+import { Localisations } from './types.ts';
+
+class Utils {
+	static readonly continuedOnNextPage: Localisations<string> = {
+		'English': 'Continued on the next page...',
+		'Polish': 'Kontynuacja na następnej stronie...',
+	};
+}
+
+export { Utils };
