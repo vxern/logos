316c5da342d062176a7dcd78aede2912b6a9b796
vxern <vxern@wordcollector.co.uk>
2022-10-14 22:02:32 +0100

    misc: Move localisations to localisations file.

--- assets/localisations/commands.ts
+++ assets/localisations/commands.ts
@@ -1,4 +1,4 @@
-import { DiscordLocalisations } from './types.ts';
+import { CommandLocalisations, DiscordLocalisations } from './types.ts';
 
 class GlobalParameters {
 	static readonly elements: DiscordLocalisations = {
@@ -55,4 +55,45 @@ class GlobalParameters {
 	};
 }
 
-export { GlobalParameters };
+class Commands {
+	static readonly information: CommandLocalisations<'bot' | 'guild'> = {
+		name: {
+			'English': 'information',
+			'Polish': 'informacje',
+			'Romanian': 'informații',
+		},
+		description: {
+			'English': 'Used to display various information.',
+			'Polish': 'Komenda używania do wyświetlania różnych informacji.',
+			'Romanian': 'Comandă utilizată pentru afișarea diverselor informații.',
+		},
+		options: {
+			bot: {
+				name: {
+					'English': 'bot',
+					'Polish': 'bot',
+					'Romanian': 'bot',
+				},
+				description: {
+					'English': 'Displays information about the bot.',
+					'Polish': 'Wyświetla informacje o bocie.',
+					'Romanian': 'Afișează informații despre bot.',
+				},
+			},
+			guild: {
+				name: {
+					'English': 'server',
+					'Polish': 'serwer',
+					'Romanian': 'server',
+				},
+				description: {
+					'English': 'Displays information about the server.',
+					'Polish': 'Wyświetla informacje o serwerze.',
+					'Romanian': 'Afișează informații despre server.',
+				},
+			},
+		},
+	};
+}
+
+export { Commands, GlobalParameters };
