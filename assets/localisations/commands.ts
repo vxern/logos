a9fa4eb7f8800f22dfed8a72a87695445396fa8b
vxern <vxern@wordcollector.co.uk>
2022-10-16 17:28:16 +0100

    misc: Move parameters into separate file.

--- assets/localisations/commands.ts
+++ assets/localisations/commands.ts
@@ -1,62 +1,7 @@
 import { User } from '../../deps.ts';
 import { links } from '../../src/constants.ts';
 import { capitalise, list } from '../../src/formatting.ts';
-import { CommandLocalisations, DiscordLocalisations } from './types.ts';
-
-class GlobalParameters {
-	static readonly elements: DiscordLocalisations = {
-		name: {
-			'English': 'number',
-			'Polish': 'liczba',
-			'Romanian': 'număr',
-		},
-		description: {
-			'English': 'The number of elements to manage.',
-			'Polish': 'Liczba elementów do zarządzania.',
-			'Romanian': 'Numărul de elemente de gestionat.',
-		},
-	};
-
-	static readonly index: DiscordLocalisations = {
-		name: {
-			'English': 'index',
-			'Polish': 'indeks',
-			'Romanian': 'index',
-		},
-		description: {
-			'English': 'The index of the element.',
-			'Polish': 'Indeks elementu.',
-			'Romanian': 'Indexul elementului.',
-		},
-	};
-
-	static readonly user: DiscordLocalisations = {
-		name: {
-			'English': 'user',
-			'Polish': 'użytkownik',
-			'Romanian': 'utilizator',
-		},
-		description: {
-			'English': 'The user\'s username, tag, ID or mention.',
-			'Polish': 'Nazwa użytkownika, jego tag, ID lub wzmianka.',
-			'Romanian':
-				'Numele de utilizator, tag-ul, ID-ul sau mențiunea utilizatorului.',
-		},
-	};
-
-	static readonly show: DiscordLocalisations = {
-		name: {
-			'English': 'show',
-			'Polish': 'wyświetl',
-			'Romanian': 'afișare',
-		},
-		description: {
-			'English': 'If set to true, the result will be shown to others.',
-			'Polish': 'Jeśli tak, rezultat będzie wyświetlony innym użytkownikom.',
-			'Romanian': 'Dacă da, rezultatul va fi afișat altor utilizatori.',
-		},
-	};
-}
+import { CommandLocalisations } from './types.ts';
 
 function typedLocalisations<
 	OptionKeys extends string,
@@ -824,4 +769,4 @@ class Commands {
 	});
 }
 
-export { Commands, GlobalParameters };
+export { Commands };
