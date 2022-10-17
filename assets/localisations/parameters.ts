788471aec2fd505a4c80ae5da4ba6e8264b4ab51
vxern <vxern@wordcollector.co.uk>
2022-10-17 23:45:58 +0100

    misc: Move strings to localisation file.

--- assets/localisations/parameters.ts
+++ assets/localisations/parameters.ts
@@ -88,6 +88,99 @@ class Parameters {
 			},
 		},
 	});
+
+	static readonly music = typedLocalisations({
+		index: {
+			name: {
+				'English': 'index',
+				'Polish': 'indeks',
+				'Romanian': 'index',
+			},
+			description: {
+				'English': 'The index of the song listing in the queue.',
+				'Polish': 'Indeks wpisu w kolejce.',
+				'Romanian': 'Indexul înregistrării în coadă.',
+			},
+		},
+		query: {
+			name: {
+				'English': 'query',
+				'Polish': 'zapytanie',
+				'Romanian': 'interogare',
+			},
+			description: {
+				'English': 'The title or a link to the song or song collection.',
+				'Polish': 'Tytuł lub link do utworu lub zbioru utworów.',
+				'Romanian':
+					'Titlul melodiei, link-ul către aceasta sau către unui set de melodii.',
+			},
+		},
+		byTimestamp: {
+			name: {
+				'English': 'by',
+				'Polish': 'o',
+				'Romanian': 'peste',
+			},
+			description: {
+				'English': 'The time period representation in the format `hh:mm:ss`.',
+				'Polish': 'Reprezentacja okresu czasowego w formacie `gg:mm:ss`.',
+				'Romanian':
+					'Reprezentarea unei perioade de timp în formatul `oo:mm:ss`.',
+			},
+		},
+		toTimestamp: {
+			name: {
+				'English': 'to',
+				'Polish': 'do',
+				'Romanian': 'către',
+			},
+			description: {
+				'English': 'The time period representation in the format `hh:mm:ss`.',
+				'Polish': 'Reprezentacja okresu czasowego w formacie `gg:mm:ss`.',
+				'Romanian':
+					'Reprezentarea unei perioade de timp în formatul `oo:mm:ss`.',
+			},
+		},
+		collection: {
+			name: {
+				'English': 'collection',
+				'Polish': 'zbiór',
+				'Romanian': 'set',
+			},
+			description: {
+				'English':
+					'If set to true, the action will be taken on the song collection instead.',
+				'Polish': 'Jeśli tak, działanie zostanie wykonane na zbiorze utworów.',
+				'Romanian': 'Dacă da, acțiunea va fi efectuată pe set de melodii.',
+			},
+		},
+		by: {
+			name: {
+				'English': 'by',
+				'Polish': 'o',
+				'Romanian': 'cu',
+			},
+			description: {
+				'English': 'The number of songs or song listings to skip by.',
+				'Polish': 'Liczba utworów lub wpisów, które mają być przewinięte.',
+				'Romanian':
+					'Numărul de melodii sau de înregistrări care să fie sărite peste.',
+			},
+		},
+		to: {
+			name: {
+				'English': 'to',
+				'Polish': 'do',
+				'Romanian': 'până-la',
+			},
+			description: {
+				'English': 'The index of the track to skip to.',
+				'Polish': 'Indeks utworu lub wpisu do którego przewinąć.',
+				'Romanian':
+					'Indexul melodiei sau al înregistrării până la care să fie sărit peste.',
+			},
+		},
+	});
 }
 
 export { Parameters };
