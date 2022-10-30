3dcb8e62f9fa2f706d20fd28e05b66197c2cbb0a
vxern <vxern@wordcollector.co.uk>
2022-10-30 22:27:47 +0000

    chore: Add sentry to dependencies and set the connection up.

--- mod.ts
+++ mod.ts
@@ -1,26 +1,27 @@
 import 'dotenv_load';
+import { Sentry } from './deps.ts';
 import { initialiseClient } from './src/client.ts';
 
 // Array of environment variables required to run the program.
 const requiredKeys = [
+	'ENVIRONMENT',
 	'DISCORD_SECRET',
 	'FAUNA_SECRET',
 	'DEEPL_SECRET',
+	'SENTRY_SECRET',
 ] as const;
 // Array of booleans indicating which environment variables are present at launch.
 const presentKeys = requiredKeys.map((key) => !!Deno.env.get(key));
 
 // If at least one environment variable is not present
 if (presentKeys.includes(false)) {
-	const missingKeys = requiredKeys.filter((_, index) =>
-		!presentKeys.at(index)!
-	);
+	const missingKeys = requiredKeys.filter((_, index) => !presentKeys.at(index)!);
 	console.error(
-		`Missing one or more required environment variables: ${
-			missingKeys.join(', ')
-		}`,
+		`Missing one or more required environment variables: ${missingKeys.join(', ')}`,
 	);
 	Deno.exit(1);
 }
+
+Sentry.init({ dsn: Deno.env.get('SENTRY_SECRET'), environment: Deno.env.get('ENVIRONMENT') });
 
 initialiseClient();
