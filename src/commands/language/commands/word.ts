feae02bd760acf6c3a3bffa2056a9085e3a0a2d6
vxern <vxern@wordcollector.co.uk>
2022-11-06 17:58:26 +0000

    refactor: Move display functions from `dictionary.ts` to `word.ts`.

--- src/commands/language/commands/word.ts
+++ src/commands/language/commands/word.ts
@@ -5,6 +5,7 @@ import {
 	ApplicationCommandOptionTypes,
 	Bot,
 	ButtonStyles,
+	DiscordEmbedField,
 	editOriginalInteractionResponse,
 	Embed,
 	Interaction,
@@ -17,9 +18,10 @@ import {
 import { Client } from '../../../client.ts';
 import { CommandBuilder } from '../../../commands/command.ts';
 import configuration from '../../../configuration.ts';
+import { BulletStyles, list } from '../../../formatting.ts';
 import { createInteractionCollector, diagnosticMentionUser, fromHex, parseArguments } from '../../../utils.ts';
 import { show } from '../../parameters.ts';
-import { DictionaryEntry, getEmbedFields } from '../data/dictionary.ts';
+import { DictionaryEntry, TaggedValue } from '../data/dictionary.ts';
 
 const command: CommandBuilder = {
 	...createLocalisations(Commands.word),
@@ -151,13 +153,7 @@ async function word(
 	const generateEmbed: () => Embed = () => {
 		const entry = entries.at(pageIndex)!;
 
-		const fields = getEmbedFields(entry, interaction.locale, { verbose: verbose ?? false });
-
-		return {
-			title: entry.word,
-			fields: fields,
-			color: fromHex('#d6e3f8'),
-		};
+		return getEmbed(entry, interaction.locale, verbose ?? false);
 	};
 
 	const generateButtons = (): MessageComponents => {
@@ -187,6 +183,105 @@ async function word(
 	};
 
 	return void generateView();
+}
+
+function getEmbed(
+	entry: DictionaryEntry,
+	locale: string | undefined,
+	verbose: boolean,
+): Embed {
+	const fields: DiscordEmbedField[] = [];
+
+	if (entry.definitions) {
+		const definitionsStringified = stringifyEntries(entry.definitions, BulletStyles.Diamond);
+		const definitionsFitted = fitStringsToFieldSize(definitionsStringified, locale, verbose);
+
+		fields.push({
+			name: localise(Commands.word.strings.fields.definitions, locale),
+			value: definitionsFitted,
+		});
+	}
+
+	if (entry.expressions) {
+		const expressionsStringified = stringifyEntries(entry.expressions, BulletStyles.Arrow);
+		const expressionsFitted = fitStringsToFieldSize(expressionsStringified, locale, verbose);
+
+		fields.push({
+			name: localise(Commands.word.strings.fields.expressions, locale),
+			value: expressionsFitted,
+		});
+	}
+
+	if (entry.etymologies) {
+		fields.push({
+			name: localise(Commands.word.strings.fields.etymology, locale),
+			value: entry.etymologies.map((etymology) => {
+				if (!etymology.tags) {
+					return `**${etymology.value}**`;
+				}
+
+				if (!etymology.value) {
+					return tagsToString(etymology.tags);
+				}
+
+				return `${tagsToString(etymology.tags)} **${etymology.value}**`;
+			}).join('\n'),
+		});
+	}
+
+	return {
+		title: entry.word,
+		fields,
+		color: fromHex('#d6e3f8'),
+	};
+}
+
+function tagsToString(tags: string[]): string {
+	return tags.map((tag) => `\`${tag}\``).join(' ');
+}
+
+function stringifyEntries(entries: TaggedValue<string>[], bulletStyle: BulletStyles): string[] {
+	const entriesStringified = entries.map((entry) => {
+		if (!entry.tags) {
+			return entry.value;
+		}
+
+		return `${tagsToString(entry.tags)} ${entry.value}`;
+	});
+	const entriesEnlisted = list(entriesStringified, bulletStyle);
+	const entriesDelisted = entriesEnlisted.split('\n');
+
+	return entriesDelisted;
+}
+
+function fitStringsToFieldSize(
+	strings: string[],
+	locale: string | undefined,
+	verbose: boolean,
+): string {
+	const overheadString = localise(Commands.word.strings.definitionsOmitted, locale)(strings.length);
+	const characterOverhead = overheadString.length + 20;
+
+	const maxCharacterCount = verbose ? 4096 : 1024;
+
+	let characterCount = 0;
+	const stringsToDisplay: string[] = [];
+	for (const [string, index] of strings.map<[string, number]>((s, i) => [s, i])) {
+		characterCount += string.length;
+
+		if (characterCount + (index + 1 === strings.length ? 0 : characterOverhead) >= maxCharacterCount) break;
+
+		stringsToDisplay.push(string);
+	}
+
+	const stringsOmitted = strings.length - stringsToDisplay.length;
+
+	let fittedString = stringsToDisplay.join('\n');
+	if (stringsOmitted !== 0) {
+		fittedString += `\n*${localise(Commands.word.strings.definitionsOmitted, locale)(stringsOmitted)}*`;
+	}
+
+	return fittedString;
 }
 
 export default command;
