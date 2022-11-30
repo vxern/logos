83bbf0fe1d737c29fc498fc6f3342c533abdac6e
Dorian Oszczęda <vxern@wordcollector.co.uk>
2022-11-30 21:39:14 +0000

    misc: Rename `/cite` command to `/rule`. (#94)

--- src/commands/moderation/commands.ts
+++ src/commands/moderation/commands.ts
@@ -1,9 +1,9 @@
-import cite from 'logos/src/commands/moderation/commands/cite.ts';
+import rule from 'logos/src/commands/moderation/commands/rule.ts';
 import pardon from 'logos/src/commands/moderation/commands/pardon.ts';
 import policy from 'logos/src/commands/moderation/commands/policy.ts';
 import timeout from 'logos/src/commands/moderation/commands/timeout.ts';
 import warn from 'logos/src/commands/moderation/commands/warn.ts';
 
-const commands = [cite, pardon, timeout, warn, policy];
+const commands = [rule, pardon, timeout, warn, policy];
 
 export default commands;
