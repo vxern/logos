c0dd5ae59152ea1c3f2a7361c39218b016b642b2
Dorian Oszczęda <vxern@wordcollector.co.uk>
2022-12-01 21:50:59 +0000

    Fix use default locale (#103)
    * refactor: Move locale objects and functions to `types.ts`.
    
    * fix: Use `defaultLocale` instead of `defaultLanguage`.

--- types.ts
+++ types.ts
@@ -1,3 +1,5 @@
+import { Locales } from 'discordeno';
+
 const supportedLanguages = [
 	'Armenian',
 	'English',
@@ -13,6 +15,31 @@ function typedDefaultLanguage<T extends Language>(language: T): T {
 
 const defaultLanguage = typedDefaultLanguage('English');
 
+const localeByLanguage:
+	& Required<Record<typeof defaultLanguage, `${Locales}`>>
+	& Partial<Record<Language, `${Locales}`>> = {
+		'English': 'en-GB',
+		'Polish': 'pl',
+		'Romanian': 'ro',
+	};
+
+const defaultLocale = localeByLanguage[defaultLanguage];
+
+const languageByLocale: Partial<Record<Locales, Language>> = {
+	'en-GB': 'English',
+	'en-US': 'English',
+	'pl': 'Polish',
+	'ro': 'Romanian',
+};
+
+function getLanguageByLocale(locale: Locales): Language | undefined {
+	return languageByLocale[locale];
+}
+
+function getLocaleByLanguage(language: Language): typeof localeByLanguage[keyof typeof localeByLanguage] {
+	return localeByLanguage[language] ?? 'en-GB';
+}
+
 enum WordTypes {
 	Noun,
 	Verb,
@@ -27,5 +54,14 @@ enum WordTypes {
 	Unknown,
 }
 
-export { defaultLanguage, supportedLanguages, WordTypes };
+export {
+	defaultLanguage,
+	defaultLocale,
+	getLanguageByLocale,
+	getLocaleByLanguage,
+	languageByLocale,
+	localeByLanguage,
+	supportedLanguages,
+	WordTypes,
+};
 export type { Language };
