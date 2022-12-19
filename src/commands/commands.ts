26ee38abb8ea1943a03b8e9bfcda8cdea54a1e2d
vxern <vxern@wordcollector.co.uk>
2022-12-19 12:57:55 +0000

    feat: Include music commands if running in the development environment.

--- src/commands/commands.ts
+++ src/commands/commands.ts
@@ -1,7 +1,7 @@
 import information from 'logos/src/commands/information/commands.ts';
 import language from 'logos/src/commands/language/commands.ts';
 import moderation from 'logos/src/commands/moderation/commands.ts';
-//import music from 'logos/src/commands/music/commands.ts';
+import music from 'logos/src/commands/music/commands.ts';
 import server from 'logos/src/commands/server/commands.ts';
 import social from 'logos/src/commands/social/commands.ts';
 
@@ -9,7 +9,7 @@ const commands = [
 	...information,
 	...language,
 	...moderation,
-	//...music,
+	...(Deno.env.get('ENVIRONMENT') === 'development' ? music : []),
 	...server,
 	...social,
 ];
