b77af98e7e6f5c43456527e21b09d387545c26ca
vxern <vxern@wordcollector.co.uk>
2022-12-31 17:15:00 +0000

    feat: Create suggestion service and activate it.

--- src/services/services.ts
+++ src/services/services.ts
@@ -3,12 +3,13 @@ import dynamicVoiceChannels from 'logos/src/services/dynamic-voice-channels.ts';
 import entry from 'logos/src/services/entry.ts';
 import notices from 'logos/src/services/notices.ts';
 import reports from 'logos/src/services/reports.ts';
+import suggestions from 'logos/src/services/suggestions.ts';
 import verification from 'logos/src/services/verification.ts';
 import { Client } from 'logos/src/client.ts';
 
 type ServiceStarter = ([client, bot]: [Client, Bot]) => void;
 
-const services = [dynamicVoiceChannels, entry, notices, reports, verification];
+const services = [dynamicVoiceChannels, entry, notices, reports, suggestions, verification];
 
 export default services;
 export type { ServiceStarter };
