ffa28894b50d803c03e24f9f9e55001f0cf4cdc2
vxern <vxern@wordcollector.co.uk>
2023-04-21 12:53:05 +0100

    feat: Add French to supported languages.

--- src/localisation/transformers.ts
+++ src/localisation/transformers.ts
@@ -1,5 +1,6 @@
 import { MessagePipeTransformer } from 'messagepipe';
 import english from 'logos/src/localisation/transformers/english.ts';
+import french from 'logos/src/localisation/transformers/french.ts';
 import hungarian from 'logos/src/localisation/transformers/hungarian.ts';
 import polish from 'logos/src/localisation/transformers/polish.ts';
 import romanian from 'logos/src/localisation/transformers/romanian.ts';
@@ -8,6 +9,7 @@ import { Language } from 'logos/types.ts';
 const transformers: Record<Language, Record<string, MessagePipeTransformer<any>>> = {
 	'Armenian': {},
 	'English': english,
+	'French': french,
 	'Hungarian': hungarian,
 	'Polish': polish,
 	'Romanian': romanian,
