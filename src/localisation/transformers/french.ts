b325b4fac77f7463e2ed7496fba60c78e828e718
Dorian Oszczęda <vxern@wordcollector.co.uk>
2023-04-23 22:26:41 +0100

    feat: Add French language and translations. (#195)
    * New Crowdin updates (#192)
    
    * New translations en-US.json (Romanian)
    
    * New translations en-US.json (French)
    
    * New translations en-US.json (Greek)
    
    * New translations en-US.json (Finnish)
    
    * New translations en-US.json (Hungarian)
    
    * New translations en-US.json (Italian)
    
    * New translations en-US.json (Dutch)
    
    * New translations en-US.json (Norwegian)
    
    * New translations en-US.json (Polish)
    
    * New translations en-US.json (Russian)
    
    * New translations en-US.json (Turkish)
    
    * New translations en-US.json (Romanian)
    
    * New translations en-US.json (French)
    
    * New translations en-US.json (Hungarian)
    
    * New translations en-US.json (Turkish)
    
    * New translations en-US.json (Romanian)
    
    * New translations en-US.json (Hungarian)
    
    * New translations en-US.json (Turkish)
    
    * New translations en-US.json (French)
    
    * New translations en-US.json (Finnish)
    
    * New translations en-US.json (Russian)
    
    * New translations en-US.json (Turkish)
    
    * New translations en-US.json (Romanian)
    
    * New translations en-US.json (French)
    
    * New translations en-US.json (Russian)
    
    * New translations en-US.json (Turkish)
    
    * New translations en-US.json (French)
    
    * New translations en-US.json (Turkish)
    
    * New translations en-US.json (Romanian)
    
    * New translations en-US.json (French)
    
    * New translations en-US.json (Greek)
    
    * New translations en-US.json (Finnish)
    
    * New translations en-US.json (Hungarian)
    
    * New translations en-US.json (Italian)
    
    * New translations en-US.json (Dutch)
    
    * New translations en-US.json (Norwegian)
    
    * New translations en-US.json (Polish)
    
    * New translations en-US.json (Russian)
    
    * New translations en-US.json (Turkish)
    
    * New translations en-US.json (Romanian)
    
    * New translations en-US.json (French)
    
    * New translations en-US.json (Hungarian)
    
    * New translations en-US.json (Turkish)
    
    * New translations en-US.json (French)
    
    * New translations en-US.json (Turkish)
    
    * New translations en-US.json (Romanian)
    
    * New translations en-US.json (French)
    
    * New translations en-US.json (Hungarian)
    
    * New translations en-US.json (Turkish)
    
    * New translations en-US.json (French)
    
    * New translations en-US.json (Hungarian)
    
    * New translations en-US.json (Turkish)
    
    * New translations en-US.json (Turkish)
    
    * fix: Missing bracket.
    
    * feat: Add French to supported languages.
    
    * New translations en-US.json (French) (#193)
    
    * misc: Remove message about processing localisations.
    
    * New translations en-US.json (French) (#194)

---
+++ src/localisation/transformers/french.ts
@@ -0,0 +1,8 @@
+function pluralise(quantity: number, { one, two }: Record<string, unknown> = {}): string {
+	if (quantity === 0 || quantity === 1) return `${quantity} ${one}`;
+
+	return `${quantity} ${two}`;
+}
+
+export { pluralise };
+export default { pluralise };
--- types.ts
+++ types.ts
@@ -3,6 +3,7 @@ import { Locales } from 'discordeno';
 const supportedLanguages = [
 	'Armenian',
 	'English',
+	'French',
 	'Hungarian',
 	'Polish',
 	'Romanian',
@@ -19,6 +20,7 @@ const localeByLanguage:
 	& Required<Record<typeof defaultLanguage, `${Locales}`>>
 	& Partial<Record<Language, `${Locales}`>> = {
 		'English': 'en-GB',
+		'French': 'fr',
 		'Hungarian': 'hu',
 		'Polish': 'pl',
 		'Romanian': 'ro',
@@ -29,6 +31,7 @@ const defaultLocale = localeByLanguage[defaultLanguage];
 const languageByLocale: Partial<Record<Locales, Language>> = {
 	'en-GB': 'English',
 	'en-US': 'English',
+	'fr': 'French',
 	'hu': 'Hungarian',
 	'pl': 'Polish',
 	'ro': 'Romanian',
