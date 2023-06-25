6a1a620b86aa48c979e659c764c46f5a7116a4dc
vxern <vxern@wordcollector.co.uk>
2023-06-25 14:59:15 +0100

    chore: Switch to NPM import.

--- src/commands/music/data/sources/youtube.ts
+++ src/commands/music/data/sources/youtube.ts
@@ -1,5 +1,5 @@
 import { Bot, Interaction, InteractionTypes, MessageComponentTypes, SelectOption } from 'discordeno';
-import { Channel, Playlist, Video, YouTube } from 'youtube';
+import * as YouTubeSearch from 'youtube-search';
 import { ListingResolver } from 'logos/src/commands/music/data/sources/sources.ts';
 import { SongListing, SongListingContentTypes } from 'logos/src/commands/music/data/types.ts';
 import { Client, localise } from 'logos/src/client.ts';
@@ -22,11 +22,11 @@ const resolver: ListingResolver = async ([client, bot], interaction, query) => {
 
 	const url = urlExpressionExecuted.at(0)!;
 	if (url.includes('list=')) {
-		const playlist = await YouTube.getPlaylist(query);
+		const playlist = await YouTubeSearch.YouTube.getPlaylist(query);
 		return fromYouTubePlaylist(playlist, interaction.user.id);
 	}
 
-	const video = await YouTube.getVideo(query);
+	const video = await YouTubeSearch.YouTube.getVideo(query);
 	return fromYouTubeVideo(video, interaction.user.id);
 };
 
@@ -35,8 +35,10 @@ async function search(
 	interaction: Interaction,
 	query: string,
 ): Promise<SongListing | undefined> {
-	const resultsAll = await YouTube.search(query, { limit: 20, type: 'all', safeSearch: false });
-	const results = resultsAll.filter((element) => isPlaylist(element) || isVideo(element)) as Array<Playlist | Video>;
+	const resultsAll = await YouTubeSearch.YouTube.search(query, { limit: 20, type: 'all', safeSearch: false });
+	const results = resultsAll.filter((element) => isPlaylist(element) || isVideo(element)) as Array<
+		YouTubeSearch.Playlist | YouTubeSearch.Video
+	>;
 	if (results.length === 0) return undefined;
 
 	return new Promise<SongListing | undefined>((resolve) => {
@@ -57,7 +59,7 @@ async function search(
 
 					const result = results.at(index)!;
 					if (isPlaylist(result)) {
-						const playlist = await YouTube.getPlaylist(result.url!);
+						const playlist = await YouTubeSearch.YouTube.getPlaylist(result.url!);
 						return resolve(fromYouTubePlaylist(playlist, interaction.user.id));
 					}
 
@@ -99,17 +101,17 @@ async function search(
 	});
 }
 
-type Result = Playlist | Video | Channel;
+type Result = YouTubeSearch.Playlist | YouTubeSearch.Video | YouTubeSearch.Channel;
 
-function isPlaylist(result: Result): result is Playlist {
+function isPlaylist(result: Result): result is YouTubeSearch.Playlist {
 	return result.type === 'playlist';
 }
 
-function isVideo(result: Result): result is Video {
+function isVideo(result: Result): result is YouTubeSearch.Video {
 	return result.type === 'video';
 }
 
-function fromYouTubeVideo(video: Video, requestedBy: bigint): SongListing | undefined {
+function fromYouTubeVideo(video: YouTubeSearch.Video, requestedBy: bigint): SongListing | undefined {
 	if (video.id === undefined) return undefined;
 
 	return {
@@ -125,7 +127,7 @@ function fromYouTubeVideo(video: Video, requestedBy: bigint): SongListing | unde
 	};
 }
 
-function fromYouTubePlaylist(playlist: Playlist, requestedBy: bigint): SongListing | undefined {
+function fromYouTubePlaylist(playlist: YouTubeSearch.Playlist, requestedBy: bigint): SongListing | undefined {
 	if (playlist.id === undefined) return undefined;
 
 	return {
