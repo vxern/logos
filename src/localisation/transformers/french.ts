85be7b61556d1059e75b0c2ee7bf13586acdc35b
vxern <vxern@wordcollector.co.uk>
2023-07-12 21:48:23 +0100

    chore: Delete deleted files.

--- src/localisation/transformers/french.ts
+++
@@ -1,8 +0,0 @@
-function pluralise(quantity: number, { one, two }: Record<string, unknown> = {}): string {
-	if (quantity === 0 || quantity === 1) return `${quantity} ${one}`;
-
-	return `${quantity} ${two}`;
-}
-
-export { pluralise };
-export default { pluralise };
