f14968c1e56f48e83de82e48817440f659ab73af
vxern <vxern@wordcollector.co.uk>
2023-12-06 21:24:35 +0000

    feat: Include PONS and Wordnik in adapter lists.

--- src/lib/commands/language/dictionaries/adapters.ts
+++ src/lib/commands/language/dictionaries/adapters.ts
@@ -1,10 +1,13 @@
-import { LearningLanguage, LocalisationLanguage } from "../../../../constants/languages";
+import { Languages, LearningLanguage, LocalisationLanguage, getFeatureLanguage } from "../../../../constants/languages";
 import defaults from "../../../../defaults";
-import { DictionaryAdapter, SearchLanguages } from "./adapter";
+import { DictionaryAdapter, DictionaryProvision } from "./adapter";
 import dexonline from "./adapters/dexonline";
 import dicolink from "./adapters/dicolink";
+import pons from "./adapters/pons";
 import wiktionary from "./adapters/wiktionary";
+import wordnik from "./adapters/wordnik";
 import wordsApi from "./adapters/words-api";
+import { createProvisionSupply } from "./dictionary-provision";
 
 type DictionaryAdapters = DictionaryAdapter[];
 type TargetDictionaryAdapters<SourceLanguage extends LocalisationLanguage> = Partial<
@@ -54,17 +57,41 @@ const adaptersLocalised: AdaptersLocalised = {
 	"English/American": {
 		primary: {
 			source: [wiktionary],
+			targets: {
+				"English/British": [wiktionary],
+			},
+		},
+		secondary: {
+			source: [wordnik, pons],
+			targets: {
+				"English/British": [wordnik, pons],
+			},
 		},
 		tertiary: {
 			source: [wordsApi],
+			targets: {
+				"English/British": [wordsApi],
+			},
 		},
 	},
 	"English/British": {
 		primary: {
 			source: [wiktionary],
+			targets: {
+				"English/American": [wiktionary],
+			},
+		},
+		secondary: {
+			source: [wordnik, pons],
+			targets: {
+				"English/American": [wordnik, pons],
+			},
 		},
 		tertiary: {
 			source: [wordsApi],
+			targets: {
+				"English/American": [wordsApi],
+			},
 		},
 	},
 	Finnish: {
@@ -83,9 +110,57 @@ const adaptersLocalised: AdaptersLocalised = {
 				"English/British": [wiktionary],
 			},
 		},
+		secondary: {
+			targets: {
+				"English/American": [pons],
+				"English/British": [pons],
+				German: [pons],
+				// "Italian": [pons],
+				Polish: [pons],
+				// "Portuguese": [pons],
+				// "Slovenian": [pons],
+				// "Spanish": [pons],
+				// "Chinese": [pons],
+			},
+		},
 	},
 	German: {
 		primary: {
+			source: [pons],
+			targets: {
+				"English/American": [pons],
+				"English/British": [pons],
+				Dutch: [pons],
+				French: [pons],
+				Finnish: [pons],
+				Greek: [pons],
+				Hungarian: [pons],
+				"Norwegian/Bokmål": [pons],
+				// "Italian": [pons],
+				Polish: [pons],
+				// "Portuguese": [pons],
+				Romanian: [pons],
+				Russian: [pons],
+				// "Slovenian": [pons],
+				// "Spanish": [pons],
+				Swedish: [pons],
+				Turkish: [pons],
+				// "Chinese": [pons],
+				// "Latin": [pons],
+				// "Elvish": [pons],
+				// "Arabic": [pons],
+				// "Persian": [pons],
+				// "Croatian": [pons],
+				// "Japanese": [pons],
+				// "Slovak": [pons],
+				// "Bulgarian": [pons],
+				// "Danish": [pons],
+				// "Czech": [pons],
+				// "Icelandic": [pons],
+				// "Serbian": [pons],
+			},
+		},
+		secondary: {
 			targets: {
 				"English/American": [wiktionary],
 				"English/British": [wiktionary],
@@ -99,12 +174,22 @@ const adaptersLocalised: AdaptersLocalised = {
 				"English/British": [wiktionary],
 			},
 		},
+		secondary: {
+			targets: {
+				German: [pons],
+			},
+		},
 	},
 	Hungarian: {
 		primary: {
 			targets: {
 				"English/American": [wiktionary],
 				"English/British": [wiktionary],
+			},
+		},
+		secondary: {
+			targets: {
+				German: [pons],
 			},
 		},
 	},
@@ -115,12 +200,28 @@ const adaptersLocalised: AdaptersLocalised = {
 				"English/British": [wiktionary],
 			},
 		},
+		secondary: {
+			targets: {
+				German: [pons],
+			},
+		},
 	},
 	Polish: {
 		primary: {
 			targets: {
 				"English/American": [wiktionary],
 				"English/British": [wiktionary],
+			},
+		},
+		secondary: {
+			targets: {
+				"English/American": [pons],
+				"English/British": [pons],
+				French: [pons],
+				// "Italian": [pons],
+				German: [pons],
+				Russian: [pons],
+				// "Spanish": [pons],
 			},
 		},
 	},
@@ -132,12 +233,25 @@ const adaptersLocalised: AdaptersLocalised = {
 				"English/British": [wiktionary],
 			},
 		},
+		secondary: {
+			targets: {
+				German: [pons],
+			},
+		},
 	},
 	Russian: {
 		primary: {
 			targets: {
 				"English/American": [wiktionary],
 				"English/British": [wiktionary],
+			},
+		},
+		secondary: {
+			targets: {
+				"English/American": [pons],
+				"English/British": [pons],
+				German: [pons],
+				Polish: [pons],
 			},
 		},
 	},
@@ -156,12 +270,22 @@ const adaptersLocalised: AdaptersLocalised = {
 				"English/British": [wiktionary],
 			},
 		},
+		secondary: {
+			targets: {
+				German: [pons],
+			},
+		},
 	},
 	Turkish: {
 		primary: {
 			targets: {
 				"English/American": [wiktionary],
 				"English/British": [wiktionary],
+			},
+		},
+		secondary: {
+			targets: {
+				German: [pons],
 			},
 		},
 	},
@@ -173,18 +297,25 @@ type AdaptersResolved = {
 	tertiary: DictionaryAdapter[];
 };
 
-function isSearchMonolingual<SourceLanguage extends LocalisationLanguage,>(
+function isSearchMonolingual<SourceLanguage extends LearningLanguage>(
 	sourceLanguage: SourceLanguage,
 	targetLanguage: LearningLanguage,
 ): targetLanguage is SourceLanguage {
-	return sourceLanguage === targetLanguage;
+	if (sourceLanguage === targetLanguage) {
+		return true;
+	}
+
+	const sourceFeatureLanguage = getFeatureLanguage(sourceLanguage);
+	const targetFeatureLanguage = getFeatureLanguage(targetLanguage);
+
+	return sourceFeatureLanguage === targetFeatureLanguage;
 }
 
 function areAdaptersMissing(adapters: AdaptersResolved): boolean {
 	return adapters.primary.length === 0 && adapters.secondary.length === 0 && adapters.tertiary.length === 0;
 }
 
-function resolveAdapters(languages: SearchLanguages): AdaptersResolved | undefined {
+function resolveAdapters(languages: Languages<LearningLanguage>): AdaptersResolved | undefined {
 	const adaptersResolved: AdaptersResolved = { primary: [], secondary: [], tertiary: [] };
 
 	const adapters = adaptersLocalised[languages.target];
@@ -227,11 +358,61 @@ function resolveAdapters(languages: SearchLanguages): AdaptersResolved | undefin
 		}
 
 		if (areAdaptersMissing(adaptersResolved)) {
-			return resolveAdapters({ source: languages.source, target: languages.source });
+			return resolveAdapters({ source: languages.target, target: languages.target });
 		}
 	}
 
 	return adaptersResolved;
+}
+
+function getAdapterSelection(
+	provisions: DictionaryProvision[],
+	adapters: DictionaryAdapter[],
+	count: number,
+): DictionaryAdapter[] {
+	const provisionSupply = createProvisionSupply();
+
+	const adapterProvisionTuples = getAdaptersSortedByProvision(adapters, provisions);
+
+	const adapterSelection: DictionaryAdapter[] = [];
+	for (const [adapter, provides] of adapterProvisionTuples) {
+		if (adapterSelection.length === count) {
+			break;
+		}
+
+		const uniqueProvisions = provides.filter((provision) => provisionSupply[provision] === 0);
+		if (uniqueProvisions.length < defaults.DICTIONARY_PROVISION_THRESHOLD) {
+			continue;
+		}
+
+		for (const provision of provides) {
+			provisionSupply[provision]++;
+		}
+
+		adapterSelection.push(adapter);
+	}
+
+	return adapterSelection;
+}
+
+function getAdaptersSortedByProvision(
+	adapters: DictionaryAdapter[],
+	missingProvisions: DictionaryProvision[],
+): [DictionaryAdapter, DictionaryProvision[]][] {
+	return adapters
+		.map<[DictionaryAdapter, DictionaryProvision[]]>((adapter) => {
+			const provides: DictionaryProvision[] = [];
+			for (const provision of adapter.provides) {
+				if (!missingProvisions.includes(provision)) {
+					continue;
+				}
+
+				provides.push(provision);
+			}
+
+			return [adapter, provides];
+		})
+		.sort(([_, a], [__, b]) => b.length - a.length);
 }
 
 function getTargetDictionaries<
@@ -254,4 +435,5 @@ function getTargetDictionaries<
 	return defaultAdapters;
 }
 
-export { resolveAdapters, isSearchMonolingual, areAdaptersMissing };
+export { resolveAdapters, isSearchMonolingual, areAdaptersMissing, getAdapterSelection };
+export type { AdaptersResolved };
