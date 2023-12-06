7546f2e739dbe74a64a52039736ad7e748a6c799
vxern <vxern@wordcollector.co.uk>
2023-12-06 21:12:31 +0000

    feat: Add word + language parameters.

--- src/lib/commands/parameters.ts
+++ src/lib/commands/parameters.ts
@@ -2,56 +2,68 @@ import * as Discord from "@discordeno/bot";
 import { OptionTemplate } from "./command";
 
 const user: OptionTemplate = {
-	name: "user",
+	name: "parameters.user",
 	type: Discord.ApplicationCommandOptionTypes.String,
 	required: true,
 	autocomplete: true,
 };
 
 const show: OptionTemplate = {
-	name: "show",
+	name: "parameters.show",
 	type: Discord.ApplicationCommandOptionTypes.Boolean,
 };
 
 const duration: OptionTemplate = {
-	name: "duration",
+	name: "parameters.duration",
 	type: Discord.ApplicationCommandOptionTypes.String,
 	required: true,
 	autocomplete: true,
 };
 
 const reason: OptionTemplate = {
-	name: "reason",
+	name: "parameters.reason",
 	type: Discord.ApplicationCommandOptionTypes.String,
 	required: true,
 };
 
 const query: OptionTemplate = {
-	name: "query",
+	name: "parameters.query",
 	type: Discord.ApplicationCommandOptionTypes.String,
 	required: true,
 };
 
 const timestamp: OptionTemplate = {
-	name: "timestamp",
+	name: "parameters.timestamp",
 	type: Discord.ApplicationCommandOptionTypes.String,
 	required: true,
 	autocomplete: true,
 };
 
 const collection: OptionTemplate = {
-	name: "collection",
+	name: "parameters.collection",
 	type: Discord.ApplicationCommandOptionTypes.Boolean,
 };
 
 const by: OptionTemplate = {
-	name: "by",
+	name: "parameters.by",
 	type: Discord.ApplicationCommandOptionTypes.Integer,
 };
 
 const to: OptionTemplate = {
-	name: "to",
+	name: "parameters.to",
 	type: Discord.ApplicationCommandOptionTypes.Integer,
 };
 
-export { by, collection, duration, query, reason, show, timestamp, to, user };
+const word: OptionTemplate = {
+	name: "parameters.word",
+	type: Discord.ApplicationCommandOptionTypes.String,
+	required: true,
+};
+
+const language: OptionTemplate = {
+	name: "parameters.language",
+	type: Discord.ApplicationCommandOptionTypes.String,
+	autocomplete: true,
+};
+
+export { by, collection, duration, query, reason, show, timestamp, to, user, word, language };
