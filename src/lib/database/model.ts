dc9a37e8a17696817e465d7e570717c6ffa3efe0
vxern <vxern@wordcollector.co.uk>
2024-02-17 22:15:22 +0000

    feat: Add more methods to `Model`.

--- src/lib/database/model.ts
+++ src/lib/database/model.ts
@@ -1,4 +1,6 @@
 import constants from "../../constants/constants";
+import { capitalise, decapitalise } from "../../formatting";
+import { Client, Database } from "../client";
 import { EntryRequest } from "./entry-request";
 import { Guild } from "./guild";
 import { GuildStats } from "./guild-stats";
@@ -9,13 +11,11 @@ import { Suggestion } from "./suggestion";
 import { Ticket } from "./ticket";
 import { User } from "./user";
 import { Warning } from "./warning";
-import {decapitalise} from "../../formatting";
-import {Client} from "../client";
 
 interface DocumentMetadata {
 	"@id": string;
 	"@collection": Collection;
-	// TODO(vxern): Put "is-deleted" here
+	"@is-deleted"?: boolean;
 }
 
 interface RawDocument {
@@ -35,12 +35,17 @@ type Collection =
 	| "Users"
 	| "Warnings";
 
-type IdentifierData<M> = M extends Model<any> ? { [K in M["_idParts"][number]]: string } : never;
-type MetadataOrIdentifierData<M> = { "@metadata": DocumentMetadata } | IdentifierData<M>;
+type IdentifierParts<M extends Model<any>> = M["_idParts"];
+type IdentifierData<M extends Model<any>> = { [K in IdentifierParts<M>[number]]: string };
+type IdentifierDataWithDummies<M extends Model<any>> = { [K in IdentifierParts<M>[number]]: string | undefined };
+type MetadataOrIdentifierData<M extends Model<any>> = { "@metadata": DocumentMetadata } | IdentifierData<M>;
 
-// TODO(vxern): Add soft-deletion.
+type ClientOrDatabase = Client | Database;
+
+const IDENTIFIER_PARTS_ORDERED: string[] = ["guildId", "userId", "authorId", "targetId", "createdAt"];
+
 abstract class Model<Generic extends { idParts: readonly string[] }> {
-	static readonly #_classes: Record<Collection, { new (parameter: any): Model<any> }> = {
+	static readonly #_classes: Record<Collection, { new (data: any): Model<any> }> = {
 		EntryRequests: EntryRequest,
 		GuildStats: GuildStats,
 		Guilds: Guild,
@@ -74,42 +79,126 @@ abstract class Model<Generic extends { idParts: readonly string[] }> {
 	constructor({ createdAt, "@metadata": metadata }: { createdAt?: number; "@metadata": DocumentMetadata }) {
 		this.createdAt = createdAt ?? Date.now();
 		this["@metadata"] = metadata;
-		this._idParts = metadata["@id"].split(constants.symbols.database.separator).slice(1) as Generic["idParts"];
+
+		const [_, idParts] = Model.getDataFromId(metadata["@id"]);
+		this._idParts = idParts;
 	}
 
-	static from<M extends Model<any>>(data: RawDocument): M {
-		if (data["@metadata"]["@collection"] === "@empty") {
-			throw `Document ${data["@metadata"]["@collection"]} is not part of any collection.`;
+	static from<M extends Model<any>>(payload: RawDocument): M {
+		if (payload["@metadata"]["@collection"] === "@empty") {
+			throw `Document ${payload["@metadata"]["@collection"]} is not part of any collection.`;
 		}
 
-		if (!(data["@metadata"]["@collection"] in Model.#_classes)) {
-			throw `Document ${data.id} is part of unknown collection: "${data["@metadata"]["@collection"]}"`;
+		if (!(payload["@metadata"]["@collection"] in Model.#_classes)) {
+			throw `Document ${payload.id} is part of unknown collection: "${payload["@metadata"]["@collection"]}"`;
 		}
 
-		const Class = Model.#_classes[data["@metadata"]["@collection"] as Collection];
+		const Class = Model.#_classes[payload["@metadata"]["@collection"] as Collection];
 
-		return new Class(data) as M;
+		return new Class(payload) as M;
 	}
 
 	static buildPartialId<M extends Model<any>>(data: IdentifierData<M>): string {
-		return Object.values(data).join(constants.symbols.database.separator);
+		const parts: string[] = [];
+		for (const part of IDENTIFIER_PARTS_ORDERED) {
+			if (!(part in data)) {
+				continue;
+			}
+
+			parts.push(data[part as keyof typeof data]);
+		}
+
+		return parts.join(constants.symbols.database.separator);
 	}
 
 	static buildId<M extends Model<any>>(data: IdentifierData<M>, { collection }: { collection: Collection }): string {
 		const collectionCamelCase = decapitalise(collection);
-		const partialId = Model.buildPartialId<M>(data);
+		const partialId = Model.buildPartialId(data);
 
 		return `${collectionCamelCase}${constants.symbols.database.separator}${partialId}`;
 	}
 
-	async update(client: Client, callback: () => void): Promise<void> {
-		await client.database.withSession(async (session) => {
+	static getDataFromId<M extends Model<any>>(id: string): [collection: Collection, data: IdentifierParts<M>] {
+		const [collectionCamelCase, ...data] = id.split(constants.symbols.database.separator);
+		const collection = capitalise(collectionCamelCase!);
+		if (!(collection in Model.#_classes)) {
+			throw `Collection "${collection}" encoded in ID "${id}" is unknown.`;
+		}
+
+		return [collection as Collection, data];
+	}
+
+	static #withDummiesReplaced<M extends Model<any>>(
+		data: IdentifierDataWithDummies<M>,
+		{ value }: { value: string },
+	): IdentifierData<M> {
+		return Object.fromEntries(Object.entries(data).map(([key, value_]) => [key, value_ ?? value])) as IdentifierData<M>;
+	}
+
+	static async all<M extends Model<any>>(
+		clientOrDatabase: ClientOrDatabase,
+		{ collection, where }: { collection: Collection; where?: IdentifierDataWithDummies<M> },
+	): Promise<M[]> {
+		const { promise, resolve } = Promise.withResolvers<M[]>();
+
+		await getDatabase(clientOrDatabase).withSession(async (session) => {
+			let query = session.query<M>({ collection });
+
+			if (where !== undefined) {
+				const clause = Model.#withDummiesReplaced(where, { value: "\\d+" });
+				query = query.whereRegex("id", Model.buildId(clause, { collection }));
+			}
+
+			const result = await query.all();
+
+			resolve(result);
+		});
+
+		return promise;
+	}
+
+	async create(clientOrDatabase: ClientOrDatabase): Promise<void> {
+		await getDatabase(clientOrDatabase).withSession(async (session) => {
+			await session.set(this);
+			await session.saveChanges();
+		});
+	}
+
+	async update(clientOrDatabase: ClientOrDatabase, callback: () => void): Promise<void> {
+		await getDatabase(clientOrDatabase).withSession(async (session) => {
 			callback();
 			await session.set(this);
 			await session.saveChanges();
 		});
 	}
+
+	async delete(clientOrDatabase: ClientOrDatabase): Promise<void> {
+		await this.update(clientOrDatabase, () => {
+			this["@metadata"]["@is-deleted"] = true;
+		});
+
+		await getDatabase(clientOrDatabase).withSession(async (session) => {
+			await session.remove(this);
+			await session.saveChanges();
+		});
+	}
+}
+
+function getDatabase(clientOrDatabase: ClientOrDatabase): Database {
+	if (clientOrDatabase instanceof Client) {
+		return clientOrDatabase.database;
+	}
+
+	return clientOrDatabase;
 }
 
 export { Model };
-export type { Collection, RawDocument, DocumentMetadata, IdentifierData, MetadataOrIdentifierData };
+export type {
+	Collection,
+	RawDocument,
+	DocumentMetadata,
+	IdentifierParts,
+	IdentifierData,
+	MetadataOrIdentifierData,
+	ClientOrDatabase,
+};
