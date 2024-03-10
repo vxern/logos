ab5359e948729bb09c647e568fb57a6a1827249a
vxern <vxern@wordcollector.co.uk>
2024-03-10 20:09:29 +0000

    misc: Prevent messages being logged during tests.

--- test/setup.ts
+++ test/setup.ts
@@ -1,3 +1,7 @@
+import log from "loglevel";
 import { registerPolyfills } from "../src/polyfills";
 
 registerPolyfills();
+
+// Silence logs to prevent undesired messages being shown during testing.
+log.disableAll();
