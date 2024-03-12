b1329e28f44615ca03c41fd47789579603183900
Dorian Oszczęda <vxern@wordcollector.co.uk>
2024-03-12 13:07:07 +0000

    test: Add skeleton test file.

---
+++ test/lib/collectors.spec.ts
@@ -0,0 +1,75 @@
+import {Collector} from "../../src/lib/collectors";
+import {expect} from "chai";
+
+describe("Collector", () => {
+    describe("close()", () => {
+        it("acts as an alias for dispatchDone().", () => {
+            // TODO(vxern): Test.
+        });
+    });
+
+    describe("constructor()", () => {
+        it("creates an object.", () => {
+            expect(() => new Collector()).to.not.throw;
+        });
+    });
+
+    describe("initialise()", () => {
+        it("initialises the collector.", () => {
+            // TODO(vxern): Test.
+        });
+    });
+
+    describe("filter()", () => {
+        it("does not filter out any incoming events by default.", () => {
+            // TODO(vxern): Test.
+        });
+    });
+
+    describe("dispatchCollect()", () => {
+        // TODO(vxern): Test.
+    });
+
+    describe("dispatchDone()", () => {
+        // TODO(vxern): Test.
+    });
+
+    describe("onCollect()", () => {
+        // TODO(vxern): Test.
+    });
+
+    describe("onDone()", () => {
+        // TODO(vxern): Test.
+    });
+});
+
+describe("InteractionCollector", () => {
+    it("constructor()", () => {
+        // TODO(vxern): Test.
+    });
+
+    it("getCommandName()", () => {
+        // TODO(vxern): Test.
+    });
+
+    it("encodeCustomId()", () => {
+        // TODO(vxern): Test.
+    });
+
+    it("filter()", () => {
+        // TODO(vxern): Test.
+    });
+
+    it("onCollect()", () => {
+        // TODO(vxern): Test.
+    });
+
+    it("encodeId()", () => {
+        // TODO(vxern): Test.
+    });
+
+    it("decodeId()", () => {
+        // TODO(vxern): Test.
+    });
+
+});
