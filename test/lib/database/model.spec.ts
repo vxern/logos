e9329c4c8b05cd2cc783c780ae7a2f2eb7185f7f
vxern <vxern@wordcollector.co.uk>
2024-03-14 23:54:00 +0000

    test: Create skeleton test file.

---
+++ test/lib/database/model.spec.ts
@@ -0,0 +1,57 @@
+describe("Model", () => {
+	describe("idParts()", () => {
+		// TODO(vxern): Test.
+	});
+
+	describe("partialId()", () => {
+		// TODO(vxern): Test.
+	});
+
+	describe("constructor()", () => {
+		// TODO(vxern): Test.
+	});
+
+	describe("buildMetadata()", () => {
+		// TODO(vxern): Add tests.
+	});
+
+	describe("buildPartialId()", () => {
+		// TODO(vxern): Add tests.
+	});
+
+	describe("buildId()", () => {
+		// TODO(vxern): Add tests.
+	});
+
+	describe("getDataFromId()", () => {
+		// TODO(vxern): Add tests.
+	});
+
+	describe("all()", () => {
+		// TODO(vxern): Add tests.
+	});
+
+	describe("crossesRateLimdescribe()", () => {
+		// TODO(vxern): Add tests.
+	});
+
+	describe("create()", () => {
+		// TODO(vxern): Add tests.
+	});
+
+	describe("update()", () => {
+		// TODO(vxern): Add tests.
+	});
+
+	describe("delete()", () => {
+		// TODO(vxern): Add tests.
+	});
+});
+
+describe("isSupportedCollection()", () => {
+	// TODO(vxern): Add tests.
+});
+
+describe("getDatabase()", () => {
+	// TODO(vxern): Add tests.
+});
