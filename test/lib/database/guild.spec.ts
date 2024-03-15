3be531c706e9df868e488e0ebb50ed2f615b132d
Dorian Oszczęda <vxern@wordcollector.co.uk>
2024-03-15 16:56:48 +0000

    test: Create skeleton test suite for `Guild`.

---
+++ test/lib/database/guild.spec.ts
@@ -0,0 +1,199 @@
+describe("Guild", () => {
+	describe("guildId()", () => {
+		// TODO(vxern): Add tests.
+	});
+
+	describe("localisationLanguage()", () => {
+		// TODO(vxern): Add tests.
+	});
+
+	describe("targetLanguage()", () => {
+		// TODO(vxern): Add tests.
+	});
+
+	describe("featureLanguage()", () => {
+		// TODO(vxern): Add tests.
+	});
+
+	describe("informationFeatures()", () => {
+		// TODO(vxern): Add tests.
+	});
+
+	describe("journalling()", () => {
+		// TODO(vxern): Add tests.
+	});
+
+	describe("noticeFeatures()", () => {
+		// TODO(vxern): Add tests.
+	});
+
+	describe("informationNotice()", () => {
+		// TODO(vxern): Add tests.
+	});
+
+	describe("resourceNotice()", () => {
+		// TODO(vxern): Add tests.
+	});
+
+	describe("roleNotice()", () => {
+		// TODO(vxern): Add tests.
+	});
+
+	describe("welcomeNotice()", () => {
+		// TODO(vxern): Add tests.
+	});
+
+	describe("languageFeatures()", () => {
+		// TODO(vxern): Add tests.
+	});
+
+	describe("answers()", () => {
+		// TODO(vxern): Add tests.
+	});
+
+	describe("corrections()", () => {
+		// TODO(vxern): Add tests.
+	});
+
+	describe("cefr()", () => {
+		// TODO(vxern): Add tests.
+	});
+
+	describe("game()", () => {
+		// TODO(vxern): Add tests.
+	});
+
+	describe("resources()", () => {
+		// TODO(vxern): Add tests.
+	});
+
+	describe("translate()", () => {
+		// TODO(vxern): Add tests.
+	});
+
+	describe("word()", () => {
+		// TODO(vxern): Add tests.
+	});
+
+	describe("targetOnly()", () => {
+		// TODO(vxern): Add tests.
+	});
+
+	describe("roleLanguages()", () => {
+		// TODO(vxern): Add tests.
+	});
+
+	describe("moderationFeatures()", () => {
+		// TODO(vxern): Add tests.
+	});
+
+	describe("alerts()", () => {
+		// TODO(vxern): Add tests.
+	});
+
+	describe("policy()", () => {
+		// TODO(vxern): Add tests.
+	});
+
+	describe("rules()", () => {
+		// TODO(vxern): Add tests.
+	});
+
+	describe("purging()", () => {
+		// TODO(vxern): Add tests.
+	});
+
+	describe("slowmode()", () => {
+		// TODO(vxern): Add tests.
+	});
+
+	describe("timeouts()", () => {
+		// TODO(vxern): Add tests.
+	});
+
+	describe("warns()", () => {
+		// TODO(vxern): Add tests.
+	});
+
+	describe("reports()", () => {
+		// TODO(vxern): Add tests.
+	});
+
+	describe("verification()", () => {
+		// TODO(vxern): Add tests.
+	});
+
+	describe("serverFeatures()", () => {
+		// TODO(vxern): Add tests.
+	});
+
+	describe("dynamicVoiceChannels()", () => {
+		// TODO(vxern): Add tests.
+	});
+
+	describe("entry()", () => {
+		// TODO(vxern): Add tests.
+	});
+
+	describe("roleIndicators()", () => {
+		// TODO(vxern): Add tests.
+	});
+
+	describe("suggestions()", () => {
+		// TODO(vxern): Add tests.
+	});
+
+	describe("resourceSubmissions()", () => {
+		// TODO(vxern): Add tests.
+	});
+
+	describe("tickets()", () => {
+		// TODO(vxern): Add tests.
+	});
+
+	describe("socialFeatures()", () => {
+		// TODO(vxern): Add tests.
+	});
+
+	describe("music()", () => {
+		// TODO(vxern): Add tests.
+	});
+
+	describe("praises()", () => {
+		// TODO(vxern): Add tests.
+	});
+
+	describe("profile()", () => {
+		// TODO(vxern): Add tests.
+	});
+
+	describe("constructor()", () => {
+		it("creates an object.", () => {
+			// TODO(vxern): Test.
+		});
+	});
+
+	describe("get()", () => {
+		// TODO(vxern): Add tests.
+	});
+
+	describe("getOrCreate()", () => {
+		// TODO(vxern): Add tests.
+	});
+
+	describe("isEnabled()", () => {
+		// TODO(vxern): Add tests.
+	});
+
+	describe("areEnabled()", () => {
+		// TODO(vxern): Add tests.
+	});
+
+	describe("isTargetLanguageOnly()", () => {
+		// TODO(vxern): Add tests.
+	});
+});
+
+describe("timeStructToMilliseconds", () => {
+	// TODO(vxern): Add tests.
+});
