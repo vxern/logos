4dc851887eb9f0176dc6961dbd6620f08d7addf1
vxern <vxern@wordcollector.co.uk>
2024-03-18 21:59:49 +0000

    misc: Improve typing on slowmode constants.

--- src/constants/slowmode.ts
+++ src/constants/slowmode.ts
@@ -3,7 +3,7 @@ import time from "./time";
 const levels = ["lowest", "low", "medium", "high", "highest", "emergency", "lockdown", "beyond"] as const;
 type SlowmodeLevel = (typeof levels)[number];
 
-const _slowmodeDelayByLevel: Record<SlowmodeLevel, number> = {
+const slowmodeDelayByLevel = Object.freeze({
 	lowest: time.second * 5,
 	low: time.second * 10,
 	medium: time.second * 30,
@@ -12,18 +12,18 @@ const _slowmodeDelayByLevel: Record<SlowmodeLevel, number> = {
 	emergency: time.minute * 20,
 	lockdown: time.hour,
 	beyond: time.day,
-};
+} as const satisfies Record<SlowmodeLevel, number>);
 
 function isValidSlowmodeLevel(level: string): level is SlowmodeLevel {
 	return (levels as readonly string[]).includes(level);
 }
 
 function getSlowmodeDelayByLevel(level: SlowmodeLevel): number {
-	return Math.floor(_slowmodeDelayByLevel[level] / 1000);
+	return Math.floor(slowmodeDelayByLevel[level] / 1000);
 }
 
 function getSlowmodeLevelByDelay(delay: number): SlowmodeLevel | undefined {
-	for (const [level, delay_] of Object.entries(_slowmodeDelayByLevel) as [SlowmodeLevel, number][]) {
+	for (const [level, delay_] of Object.entries(slowmodeDelayByLevel) as [SlowmodeLevel, number][]) {
 		if (delay === delay_) {
 			return level;
 		}
