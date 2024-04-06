1f7edaa3ed620406a9b28ecb7d6f5de463c5a1af
Dorian Oszczęda <vxern@wordcollector.co.uk>
2024-04-06 22:18:15 +0100

    chore: Add missing properties to environment mock.

--- test/mocks.ts
+++ test/mocks.ts
@@ -2,16 +2,20 @@ import { Client, Environment } from "logos/client";
 import { ServiceStore } from "logos/stores/services";
 
 const mockEnvironment: Environment = {
-	deeplSecret: "DEEPL_SECRET",
-	discordSecret: "MTIxNjQ2Njk1NjQ1OTE4NDE0OA.GAQb1m.RnwgsOMPEKyOm7QF5NPkIaq4X93bTL6rV-KrsU",
 	isDebug: false,
+	discordSecret: "MTIxNjQ2Njk1NjQ1OTE4NDE0OA.GAQb1m.RnwgsOMPEKyOm7QF5NPkIaq4X93bTL6rV-KrsU",
+	deeplSecret: "DEEPL_SECRET",
+	rapidApiSecret: "SECRET_RAPID_API",
+	ravendbDatabase: "logos",
+	ravendbPort: "8080",
+	ravendbHost: "http://127.0.0.1",
+	ravendbSecure: false,
+	redisHost: "http://127.0.0.1",
+	redisPort: "6379",
+	redisPassword: "",
 	lavalinkHost: "127.0.0.1",
 	lavalinkPassword: "password123",
 	lavalinkPort: "7031",
-	rapidApiSecret: "SECRET_RAPID_API",
-	ravendbDatabase: "logos",
-	ravendbHost: "http://127.0.0.1",
-	ravendbSecure: false,
 };
 const mockClient = await Client.create({ environment: mockEnvironment, localisations: new Map() });
 
