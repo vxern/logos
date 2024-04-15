c1edde00f9fdc81458a514bef62763ec05f25a3e
Dorian Oszczęda <vxern@wordcollector.co.uk>
2024-04-15 12:51:30 +0100

    feat: Implement rough first draft for a context builder.

--- src/lib/commands/handlers/information/bot.ts
+++ src/lib/commands/handlers/information/bot.ts
@@ -1,9 +1,18 @@
+import { Locale } from "logos:constants/languages";
 import { list } from "logos:core/formatting";
 import { Client } from "logos/client";
 
-async function handleDisplayBotInformation(client: Client, interaction: Logos.Interaction): Promise<void> {
-	const locale = interaction.locale;
+type ContextBuilder<T> = ({ l, locale }: { l: Client["localise"]; locale: Locale }) => T;
+async function withContext<T>(
+	{ localise, locale, builder }: { localise: Client["localise"]; locale: Locale; builder: ContextBuilder<T> },
+	action: ({ strings }: { strings: T }) => Promise<void>,
+): Promise<void> {
+	const strings = builder({ l: localise, locale });
 
+	await action({ strings });
+}
+
+async function handleDisplayBotInformation(client: Client, interaction: Logos.Interaction): Promise<void> {
 	const botUser =
 		client.entities.users.get(client.bot.id) ??
 		(await client.bot.rest.getUser(client.bot.id).catch(() => {
@@ -14,59 +23,66 @@ async function handleDisplayBotInformation(client: Client, interaction: Logos.In
 		return;
 	}
 
-	const strings = {
-		concept: {
-			title: client.localise("information.options.bot.strings.concept.title", locale)(),
-			description: client.localise("information.options.bot.strings.concept.description", locale)(),
-		},
-		function: {
-			title: client.localise("information.options.bot.strings.function.title", locale)(),
-			description: client.localise("information.options.bot.strings.function.description", locale)(),
-			features: {
-				definitions: client.localise("information.options.bot.strings.function.features.definitions", locale)(),
-				translations: client.localise("information.options.bot.strings.function.features.translations", locale)(),
-				games: client.localise("information.options.bot.strings.function.features.games", locale)(),
-				messages: client.localise("information.options.bot.strings.function.features.messages", locale)(),
-				guides: client.localise("information.options.bot.strings.function.features.guides", locale)(),
-			},
-		},
-		languages: {
-			title: client.localise("information.options.bot.strings.languages.title", locale)(),
-			description: client.localise("information.options.bot.strings.languages.description", locale)(),
-		},
-	};
-
-	const featuresFormatted = list([
-		`${constants.emojis.bot.features.definitions} ${strings.function.features.definitions}`,
-		`${constants.emojis.bot.features.translations} ${strings.function.features.translations}`,
-		`${constants.emojis.bot.features.games} ${strings.function.features.games}`,
-		`${constants.emojis.bot.features.messages} ${strings.function.features.messages}`,
-		`${constants.emojis.bot.features.guides} ${strings.function.features.guides}`,
-	]);
-
-	await client.notice(interaction, {
-		author: {
-			iconUrl: Discord.avatarUrl(client.bot.id, botUser.discriminator, {
-				avatar: botUser.avatar ?? undefined,
-				format: "png",
+	await withContext(
+		{
+			localise: client.localise.bind(client),
+			locale: interaction.locale,
+			builder: ({ l, locale }) => ({
+				concept: {
+					title: l("information.options.bot.strings.concept.title", locale)(),
+					description: l("information.options.bot.strings.concept.description", locale)(),
+				},
+				function: {
+					title: l("information.options.bot.strings.function.title", locale)(),
+					description: l("information.options.bot.strings.function.description", locale)(),
+					features: {
+						definitions: l("information.options.bot.strings.function.features.definitions", locale)(),
+						translations: l("information.options.bot.strings.function.features.translations", locale)(),
+						games: l("information.options.bot.strings.function.features.games", locale)(),
+						messages: l("information.options.bot.strings.function.features.messages", locale)(),
+						guides: l("information.options.bot.strings.function.features.guides", locale)(),
+					},
+				},
+				languages: {
+					title: l("information.options.bot.strings.languages.title", locale)(),
+					description: l("information.options.bot.strings.languages.description", locale)(),
+				},
 			}),
-			name: botUser.username,
 		},
-		fields: [
-			{
-				name: `${constants.emojis.information.bot} ${strings.concept.title}`,
-				value: strings.concept.description,
-			},
-			{
-				name: `${constants.emojis.information.function} ${strings.function.title}`,
-				value: `${strings.function.description}\n${featuresFormatted}`,
-			},
-			{
-				name: `${constants.emojis.information.languages} ${strings.languages.title}`,
-				value: strings.languages.description,
-			},
-		],
-	});
+		async ({ strings }) => {
+			const featuresFormatted = list([
+				`${constants.emojis.bot.features.definitions} ${strings.function.features.definitions}`,
+				`${constants.emojis.bot.features.translations} ${strings.function.features.translations}`,
+				`${constants.emojis.bot.features.games} ${strings.function.features.games}`,
+				`${constants.emojis.bot.features.messages} ${strings.function.features.messages}`,
+				`${constants.emojis.bot.features.guides} ${strings.function.features.guides}`,
+			]);
+
+			await client.notice(interaction, {
+				author: {
+					iconUrl: Discord.avatarUrl(client.bot.id, botUser.discriminator, {
+						avatar: botUser.avatar ?? undefined,
+						format: "png",
+					}),
+					name: botUser.username,
+				},
+				fields: [
+					{
+						name: `${constants.emojis.information.bot} ${strings.concept.title}`,
+						value: strings.concept.description,
+					},
+					{
+						name: `${constants.emojis.information.function} ${strings.function.title}`,
+						value: `${strings.function.description}\n${featuresFormatted}`,
+					},
+					{
+						name: `${constants.emojis.information.languages} ${strings.languages.title}`,
+						value: strings.languages.description,
+					},
+				],
+			});
+		},
+	);
 }
 
 export { handleDisplayBotInformation };
