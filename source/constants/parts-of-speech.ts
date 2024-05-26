a6eb0a9cdbce08d3912585a436ea0dbb1091c335
Dorian Oszczęda <vxern@wordcollector.co.uk>
2024-05-26 13:48:52 +0100

    style: Format.

--- source/constants/parts-of-speech.ts
+++ source/constants/parts-of-speech.ts
@@ -4,31 +4,31 @@ import french from "logos:constants/parts-of-speech/french";
 import romanian from "logos:constants/parts-of-speech/romanian";
 
 const partsOfSpeech = [
-	"noun"
-,	"verb"
-,	"adjective"
-,	"adverb"
-,	"adposition"
-,	"article"
-,	"proper-noun"
-,	"letter"
-,	"character"
-,	"phrase"
-,	"idiom"
-,	"symbol"
-,	"syllable"
-,	"numeral"
-,	"initialism"
-,	"particle"
-,	"punctuation"
-,	"affix"
-,	"pronoun"
-,	"determiner"
-,	"conjunction"
-,	"interjection"
-,	"unknown"
-,] as const;
-type PartOfSpeech = typeof partsOfSpeech[number];
+	"noun",
+	"verb",
+	"adjective",
+	"adverb",
+	"adposition",
+	"article",
+	"proper-noun",
+	"letter",
+	"character",
+	"phrase",
+	"idiom",
+	"symbol",
+	"syllable",
+	"numeral",
+	"initialism",
+	"particle",
+	"punctuation",
+	"affix",
+	"pronoun",
+	"determiner",
+	"conjunction",
+	"interjection",
+	"unknown",
+] as const;
+type PartOfSpeech = (typeof partsOfSpeech)[number];
 
 function isPartOfSpeech(partOfSpeech: string): partOfSpeech is PartOfSpeech {
 	return (partsOfSpeech as readonly string[]).includes(partOfSpeech);
@@ -60,10 +60,9 @@ function getPartOfSpeech({
 		return ["unknown", terms.exact];
 	}
 
-	const partsOfSpeechLocalised = partsOfSpeechByLanguage[learningLanguage as keyof typeof partsOfSpeechByLanguage] as Record<
-		string,
-		PartOfSpeech
-	>;
+	const partsOfSpeechLocalised = partsOfSpeechByLanguage[
+		learningLanguage as keyof typeof partsOfSpeechByLanguage
+	] as Record<string, PartOfSpeech>;
 
 	if (terms.exact in partsOfSpeechLocalised) {
 		return [partsOfSpeechLocalised[terms.exact]!, terms.exact];
