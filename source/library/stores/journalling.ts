b89566d6d922aef5d48d58692dc067e6e259f6c5
Dorian Oszczęda <vxern@wordcollector.co.uk>
2024-07-04 21:58:00 +0100

    refactor: Move delegates into their own methods.

--- source/library/stores/journalling.ts
+++ source/library/stores/journalling.ts
@@ -33,34 +33,12 @@ class JournallingStore {
 	async setup(): Promise<void> {
 		this.log.info("Setting up journalling store...");
 
-		this.#guildBanAddCollector.onCollect((user, guildId) =>
-			this.tryLog("guildBanAdd", { guildId, args: [user, guildId] }),
-		);
-		this.#guildBanRemoveCollector.onCollect((user, guildId) =>
-			this.tryLog("guildBanRemove", { guildId, args: [user, guildId] }),
-		);
-		this.#guildMemberAddCollector.onCollect((member, user) =>
-			this.tryLog("guildMemberAdd", { guildId: member.guildId, args: [member, user] }),
-		);
-		this.#guildMemberRemoveCollector.onCollect((user, guildId) =>
-			this.tryLog("guildMemberRemove", { guildId, args: [user, guildId] }),
-		);
-		this.#messageDeleteCollector.onCollect((payload, message) => {
-			const guildId = payload.guildId;
-			if (guildId === undefined) {
-				return;
-			}
-
-			this.tryLog("messageDelete", { guildId, args: [payload, message] });
-		});
-		this.#messageUpdateCollector.onCollect((message, oldMessage) => {
-			const guildId = message.guildId;
-			if (guildId === undefined) {
-				return;
-			}
-
-			this.tryLog("messageUpdate", { guildId, args: [message, oldMessage] });
-		});
+		this.#guildBanAddCollector.onCollect(this.#guildBanAdd.bind(this));
+		this.#guildBanRemoveCollector.onCollect(this.#guildBanRemove.bind(this));
+		this.#guildMemberAddCollector.onCollect(this.#guildMemberAdd.bind(this));
+		this.#guildMemberRemoveCollector.onCollect(this.#guildMemberRemove.bind(this));
+		this.#messageDeleteCollector.onCollect(this.#messageDelete.bind(this));
+		this.#messageUpdateCollector.onCollect(this.#messageUpdate.bind(this));
 
 		await this.#client.registerCollector("guildBanAdd", this.#guildBanAddCollector);
 		await this.#client.registerCollector("guildBanRemove", this.#guildBanRemoveCollector);
@@ -138,6 +116,43 @@ class JournallingStore {
 			.catch((reason) =>
 				this.log.warn(`Failed to log '${event}' event on ${this.#client.diagnostics.guild(guildId)}:`, reason),
 			);
+	}
+
+	async #guildBanAdd(user: Discord.User, guildId: bigint): Promise<void> {
+		await this.tryLog("guildBanAdd", { guildId, args: [user, guildId] });
+	}
+
+	async #guildBanRemove(user: Discord.User, guildId: bigint): Promise<void> {
+		await this.tryLog("guildBanRemove", { guildId, args: [user, guildId] });
+	}
+
+	async #guildMemberAdd(member: Discord.Member, user: Discord.User): Promise<void> {
+		await this.tryLog("guildMemberAdd", { guildId: member.guildId, args: [member, user] });
+	}
+
+	async #guildMemberRemove(user: Discord.User, guildId: bigint): Promise<void> {
+		await this.tryLog("guildMemberRemove", { guildId, args: [user, guildId] });
+	}
+
+	async #messageDelete(
+		payload: Discord.Events["messageDelete"][0],
+		message: Discord.Message | undefined,
+	): Promise<void> {
+		const guildId = payload.guildId;
+		if (guildId === undefined) {
+			return;
+		}
+
+		await this.tryLog("messageDelete", { guildId, args: [payload, message] });
+	}
+
+	async #messageUpdate(message: Discord.Message, oldMessage: Discord.Message | undefined): Promise<void> {
+		const guildId = message.guildId;
+		if (guildId === undefined) {
+			return;
+		}
+
+		await this.tryLog("messageUpdate", { guildId, args: [message, oldMessage] });
 	}
 }
 
