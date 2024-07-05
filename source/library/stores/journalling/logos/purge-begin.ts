8a388b8453d3b80154422a061f9cd84a4037e7a6
Dorian Oszczęda <vxern@wordcollector.co.uk>
2024-07-06 00:00:03 +0100

    fix: Invalid parameter passed to string template.

--- source/library/stores/journalling/logos/purge-begin.ts
+++ source/library/stores/journalling/logos/purge-begin.ts
@@ -10,7 +10,7 @@ const logger: EventLogger<"purgeBegin"> = (client, [member, channel, messageCoun
 				color: constants.colours.warning,
 				description: strings.description({
 					moderator: client.diagnostics.member(member),
-					message_count: client.pluralise("events.purgeBegin.description.messages", guildLocale, {
+					messages: client.pluralise("events.purgeBegin.description.messages", guildLocale, {
 						quantity: messageCount,
 					}),
 					channel: mention(channel.id, { type: "channel" }),
--- source/library/stores/journalling/logos/purge-end.ts
+++ source/library/stores/journalling/logos/purge-end.ts
@@ -10,7 +10,7 @@ const logger: EventLogger<"purgeEnd"> = (client, [member, channel, messageCount,
 				color: constants.colours.success,
 				description: strings.description({
 					moderator: client.diagnostics.member(member),
-					message_count: client.pluralise("events.purgeBegin.description.messages", guildLocale, {
+					messages: client.pluralise("events.purgeBegin.description.messages", guildLocale, {
 						quantity: messageCount,
 					}),
 					channel: mention(channel.id, { type: "channel" }),
