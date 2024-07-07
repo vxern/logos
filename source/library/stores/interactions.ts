f40ea5e2f629dc14c5b3624443f5991b6b8eff6e
Dorian Oszczęda <vxern@wordcollector.co.uk>
2024-07-07 12:11:24 +0100

    fix: Embed colours being overwritten.

--- source/library/stores/interactions.ts
+++ source/library/stores/interactions.ts
@@ -552,9 +552,17 @@ function getInteractionCallbackData(embedOrData: EmbedOrCallbackData): Discord.I
 
 function setCallbackColour(embedOrData: EmbedOrCallbackData, { colour }: { colour: number }): EmbedOrCallbackData {
 	if (isEmbed(embedOrData)) {
+		if (embedOrData.color !== undefined) {
+			return embedOrData;
+		}
+
 		embedOrData.color = colour;
 	} else if (embedOrData.embeds !== undefined) {
 		for (const embed of embedOrData.embeds) {
+			if (embed.color !== undefined) {
+				continue;
+			}
+
 			embed.color = colour;
 		}
 	}
