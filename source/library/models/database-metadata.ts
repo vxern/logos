24bdaaf1a288059223e50812c10a4294f3da045d
Dorian Oszczęda <vxern@wordcollector.co.uk>
2024-08-26 15:48:36 +0100

    fix: Database metadata model.

--- source/library/models/database-metadata.ts
+++ source/library/models/database-metadata.ts
@@ -1,4 +1,3 @@
-import { Client } from "logos/client";
 import type { DatabaseMetadataDocument } from "logos/models/documents/database-metadata/latest";
 import { type ClientOrDatabaseStore, type CreateModelOptions, Model, getDatabase } from "logos/models/model";
 import type { DatabaseStore } from "logos/stores/database";
@@ -16,33 +15,21 @@ class DatabaseMetadata extends Model<{ collection: "DatabaseMetadata"; idParts: 
 		this.migrations = migrations;
 	}
 
-	static async get(clientOrDatabase: ClientOrDatabaseStore): Promise<DatabaseMetadata | undefined> {
-		if (clientOrDatabase instanceof Client) {
-			return clientOrDatabase.database.metadata;
-		}
-
-		return clientOrDatabase.withSession(async (session) => {
-			return session.get<DatabaseMetadata>(
-				Model.buildId<DatabaseMetadata>({}, { collection: "DatabaseMetadata" }),
-			);
-		});
-	}
-
 	static async getOrCreate(
 		clientOrDatabase: ClientOrDatabaseStore,
 		data: CreateDatabaseMetadataOptions,
 	): Promise<DatabaseMetadata> {
-		const databaseMetadataDocument = await DatabaseMetadata.get(clientOrDatabase);
-		if (databaseMetadataDocument !== undefined) {
-			return databaseMetadataDocument;
-		}
-
 		const database = getDatabase(clientOrDatabase);
+		return database.withSession(async (session) => {
+			const userDocument = await session.get<DatabaseMetadata>(
+				Model.buildId<DatabaseMetadata>(data, { collection: "DatabaseMetadata" }),
+			);
+			if (userDocument !== undefined) {
+				return userDocument;
+			}
 
-		const ticketDocument = new DatabaseMetadata(database, data);
-		await ticketDocument.create(database);
-
-		return ticketDocument;
+			return session.set(new DatabaseMetadata(database, data));
+		});
 	}
 }
 
