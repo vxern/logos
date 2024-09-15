af9df1a49d98a61ec1ae81d6dfb968092289695b
Dorian Oszczęda <vxern@wordcollector.co.uk>
2024-09-15 14:23:07 +0100

    refactor: Move migration logic into `migrator.ts`.

---
+++ source/runners/migrator.ts
@@ -0,0 +1,121 @@
+import constants from "logos:constants/constants.ts";
+import type { DatabaseMetadata } from "logos/models/database-metadata.ts";
+import type { DatabaseStore } from "logos/stores/database.ts";
+import type pino from "pino";
+
+async function migrate({
+	log,
+	database,
+	metadata,
+	availableMigrations,
+}: {
+	log: pino.Logger;
+	database: DatabaseStore;
+	metadata: DatabaseMetadata;
+	availableMigrations: AvailableMigrations;
+}): Promise<void> {
+	const completeMigrations = new Set(metadata.migrations);
+	const migrationsToExecute = Object.entries(availableMigrations).filter(
+		(migration) => !completeMigrations.has(migration[0]),
+	);
+	if (migrationsToExecute.length === 0) {
+		log.info("Migrations up to date!");
+
+		process.exit(0);
+	}
+
+	log.info(`Found ${migrationsToExecute.length} migration(s) to execute.`);
+
+	for (const [migration, filename] of migrationsToExecute) {
+		const module: { up(database: DatabaseStore): Promise<void> } = await import(
+			`../../../${constants.directories.migrations}/${filename}`
+		);
+
+		log.info(`Executing ${filename}...`);
+
+		try {
+			await module.up(database);
+		} catch (error) {
+			log.error(`Failed to run migration '${filename}': ${error}`);
+
+			process.exit(1);
+		}
+
+		await metadata.update(database, () => {
+			metadata.migrations.push(migration);
+		});
+	}
+
+	log.info("Migrated!");
+}
+
+type Migration = [migration: string, filename: string];
+async function rollback({
+	log,
+	database,
+	metadata,
+	availableMigrations,
+	step,
+}: {
+	log: pino.Logger;
+	database: DatabaseStore;
+	metadata: DatabaseMetadata;
+	availableMigrations: AvailableMigrations;
+	step: number;
+}): Promise<void> {
+	const migrationsToRollback = metadata.migrations;
+	if (migrationsToRollback.length === 0) {
+		log.info("There are no migrations to roll back.");
+
+		process.exit(0);
+	}
+
+	log.info(`Found ${migrationsToRollback.length} migration(s) to roll back.`);
+
+	const migrations = migrationsToRollback
+		.map<Migration>((migration) => {
+			const filename = availableMigrations[migration];
+			if (filename === undefined) {
+				log.error(`Could not find file for migration '${migration}'. Does it exist?`);
+
+				process.exit(1);
+			}
+
+			return [migration, filename];
+		})
+		.toReversed()
+		.slice(0, step);
+
+	for (const [_, filename] of migrations) {
+		const module: { down(database: DatabaseStore): Promise<void> } = await import(
+			`../../../${constants.directories.migrations}/${filename}`
+		);
+
+		log.info(`Rolling back ${filename}...`);
+
+		try {
+			await module.down(database);
+		} catch (error) {
+			log.error(`Failed to roll back ${filename}: ${error}`);
+
+			process.exit(1);
+		}
+
+		await metadata.update(database, () => {
+			metadata.migrations.pop();
+		});
+	}
+
+	log.info("Rolled back!");
+}
+
+type AvailableMigrations = Record<string, string>;
+async function getAvailableMigrations(): Promise<AvailableMigrations> {
+	const migrationFilenames = await Array.fromAsync(new Bun.Glob("*.ts").scan(constants.directories.migrations)).then(
+		(filenames) => filenames.toSorted(),
+	);
+
+	return Object.fromEntries(migrationFilenames.map((filename) => [filename.split("_").at(0)!, filename]));
+}
+
+export { migrate, rollback, getAvailableMigrations };
--- source/scripts/database/migrate.ts
+++ source/scripts/database/migrate.ts
@@ -1,11 +1,11 @@
 import { parseArgs } from "node:util";
-import constants from "logos:constants/constants.ts";
 import { loadEnvironment } from "logos:core/loaders/environment.ts";
 import { silent } from "logos:core/utilities.ts";
 import bun from "bun";
-import { DatabaseMetadata } from "logos/models/database-metadata.ts";
 import { DatabaseStore } from "logos/stores/database.ts";
 import pino from "pino";
+import { getAvailableMigrations, migrate, rollback } from "logos:core/runners/migrator.ts";
+import { DatabaseMetadata } from "logos/models/database-metadata.ts";
 
 const log = pino();
 
@@ -30,109 +30,24 @@ if (values.step !== undefined && !Number.isSafeInteger(Number(values.step))) {
 	process.exit(1);
 }
 
-const direction = values.rollback ? "down" : "up";
-const step = values.step !== undefined ? Number(values.step) : 1;
-
-log.info("Checking for migrations...");
-
 const environment = loadEnvironment({ log: silent });
 const database = await DatabaseStore.create({ log: silent, environment });
 await database.setup({ prefetchDocuments: false });
 
-type AvailableMigrations = Record<string, string>;
-async function getAvailableMigrations(): Promise<AvailableMigrations> {
-	const migrationFilenames = await Array.fromAsync(new Bun.Glob("*.ts").scan(constants.directories.migrations)).then(
-		(filenames) => filenames.toSorted(),
-	);
-
-	return Object.fromEntries(migrationFilenames.map((filename) => [filename.split("_").at(0)!, filename]));
-}
-
 const availableMigrations = await getAvailableMigrations();
-const metadata =
-	(await DatabaseMetadata.get(database)) ??
-	(await DatabaseMetadata.create(database, { migrations: Object.keys(availableMigrations) }));
+const metadata = await DatabaseMetadata.getOrCreate(database, { migrations: Object.keys(availableMigrations) });
 
-type Migration = [migration: string, filename: string];
-if (direction === "up") {
-	const completeMigrations = new Set(metadata.migrations);
-	const migrationsToExecute = Object.entries(availableMigrations).filter(
-		(migration) => !completeMigrations.has(migration[0]),
-	);
-	if (migrationsToExecute.length === 0) {
-		log.info("Migrations up to date!");
-
-		process.exit(0);
+const direction = values.rollback ? "down" : "up";
+switch (direction) {
+	case "up": {
+		await migrate({ log, database, metadata, availableMigrations });
+		break;
 	}
-
-	log.info(`Found ${migrationsToExecute.length} migration(s) to execute.`);
-
-	for (const [migration, filename] of migrationsToExecute) {
-		const module: { up(database: DatabaseStore): Promise<void> } = await import(
-			`../../../${constants.directories.migrations}/${filename}`
-		);
-
-		log.info(`Executing ${filename}...`);
-
-		try {
-			await module.up(database);
-		} catch (error) {
-			log.error(`Failed to run migration '${filename}': ${error}`);
-
-			process.exit(1);
-		}
-
-		await metadata.update(database, () => {
-			metadata.migrations.push(migration);
-		});
+	case "down": {
+		const step = values.step !== undefined ? Number(values.step) : 1;
+		await rollback({ log, database, metadata, availableMigrations, step });
+		break;
 	}
-
-	log.info("Migrated!");
-} else {
-	const migrationsToRollback = metadata.migrations;
-	if (migrationsToRollback.length === 0) {
-		log.info("There are no migrations to roll back.");
-
-		process.exit(0);
-	}
-
-	log.info(`Found ${migrationsToRollback.length} migration(s) to roll back.`);
-
-	const migrations = migrationsToRollback
-		.map<Migration>((migration) => {
-			const filename = availableMigrations[migration];
-			if (filename === undefined) {
-				log.error(`Could not find file for migration '${migration}'. Does it exist?`);
-
-				process.exit(1);
-			}
-
-			return [migration, filename];
-		})
-		.toReversed()
-		.slice(0, step);
-
-	for (const [_, filename] of migrations) {
-		const module: { down(database: DatabaseStore): Promise<void> } = await import(
-			`../../../${constants.directories.migrations}/${filename}`
-		);
-
-		log.info(`Rolling back ${filename}...`);
-
-		try {
-			await module.down(database);
-		} catch (error) {
-			log.error(`Failed to roll back ${filename}: ${error}`);
-
-			process.exit(1);
-		}
-
-		await metadata.update(database, () => {
-			metadata.migrations.pop();
-		});
-	}
-
-	log.info("Rolled back!");
 }
 
 process.exit(0);
