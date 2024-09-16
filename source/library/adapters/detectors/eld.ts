ab2178b6a5e1cc3014c1817e85a6ea8682d0bc3c
Dorian Oszczęda <vxern@wordcollector.co.uk>
2024-09-16 22:38:09 +0100

    chore: Add TS declaration file for `eld`.

--- source/library/adapters/detectors/eld.ts
+++ source/library/adapters/detectors/eld.ts
@@ -1,5 +1,4 @@
 import { getELDLanguageByLocale, isELDLocale } from "logos:constants/languages/detection";
-// @ts-expect-error: ELD is ES2015.
 import { eld } from "eld";
 import { DetectorAdapter, type SingleDetectionResult } from "logos/adapters/detectors/adapter";
 import type { Client } from "logos/client";
