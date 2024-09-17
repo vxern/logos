1fa8344b8de04717867ecd4ec794c802e19b04a7
Dorian Oszczęda <vxern@wordcollector.co.uk>
2024-09-17 20:04:02 +0100

    refactor: Consolidate transformers.

--- source/constants/transformers/turkish.ts
+++
@@ -1,9 +0,0 @@
-function pluralise(_: string, { one: singular }: Record<string, string> = {}): string | undefined {
-	if (singular === undefined) {
-		return undefined;
-	}
-
-	return singular;
-}
-
-export default { pluralise };
