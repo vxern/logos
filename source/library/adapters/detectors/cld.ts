19539e8718727999a5546c370a42e80c00749c6e
Dorian Oszczęda <vxern@wordcollector.co.uk>
2024-10-06 15:13:48 +0100

    feat: Remove unnecessary error mesage.

--- source/library/adapters/detectors/cld.ts
+++ source/library/adapters/detectors/cld.ts
@@ -9,10 +9,7 @@ class CLDAdapter extends DetectorAdapter {
 	}
 
 	async detect({ text }: { text: string }): Promise<SingleDetectionResult | undefined> {
-		const result = await cld.detect(text).catch((error) => {
-			this.log.error(error, "Failed to detect language.");
-			return undefined;
-		});
+		const result = await cld.detect(text).catch((_) => undefined);
 
 		const detectedLocale = result?.languages.at(0)?.code;
 		if (detectedLocale === undefined || !isCLDLocale(detectedLocale)) {
