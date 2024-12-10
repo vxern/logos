73989808dc1909a97d763a69826aaab9c4ed9187
Dorian Oszczęda <vxern@wordcollector.co.uk>
2024-12-10 12:23:06 +0000

    feat: Add word sigils to guild document.

---
+++ migrations/20241109204051_add-word-sigils-service.ts
@@ -0,0 +1,23 @@
+import type { GuildDocument as Previous } from "logos/models/documents/guild/4.48.0";
+import type { GuildDocument as Next } from "logos/models/documents/guild/latest";
+import type { Model } from "logos/models/model";
+import type { DatabaseStore } from "logos/stores/database";
+
+// This block is executed when the migration is enacted.
+async function up(database: DatabaseStore): Promise<void> {
+	const documents = await database.withSession(async (session) => {
+		return session.query<Model & Previous>({ collection: "Guilds" }).run();
+	});
+
+	for (const previous of documents) {
+		const next = previous as Model & Next;
+		await next.update(database, async () => {
+			next.enabledFeatures.wordSigils = next.enabledFeatures.word;
+		});
+	}
+}
+
+// This block is executed when the migration is rolled back.
+function down(_: DatabaseStore): void {}
+
+export { up, down };
