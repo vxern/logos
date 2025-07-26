4a2eb3c5068b2bb49f1982fa2f2d7ee2d7e450c6
Dorian Oszczęda <vxern@wordcollector.co.uk>
2025-07-26 13:40:11 +0100

    fix: Status not registering.

--- source/library/services/status.ts
+++ source/library/services/status.ts
@@ -1,12 +1,23 @@
 import type { Client } from "rost/client";
+import { Collector } from "rost/collectors";
 import { GlobalService } from "rost/services/service";
 
 class StatusService extends GlobalService {
+	readonly #readies: Collector<"ready">;
+
 	constructor(client: Client) {
 		super(client, { identifier: "StatusService" });
+
+		this.#readies = new Collector();
 	}
 
 	async start(): Promise<void> {
+		this.#readies.onCollect(this.#setBotStatus.bind(this));
+
+		this.client.registerCollector("ready", this.#readies);
+	}
+
+	async #setBotStatus(): Promise<void> {
 		await this.client.bot.gateway
 			.editBotStatus({
 				activities: [
@@ -15,7 +26,7 @@ class StatusService extends GlobalService {
 						name: "Status",
 						type: Discord.ActivityTypes.Custom,
 						// TODO(vxern): Localise.
-						state: "Managing Learn Romanian",
+						state: "Managing 🇷🇴 Learn Romanian",
 					},
 				],
 				status: "online",
